13d4c42385e731f42305af7f6145849494e2d4fa
varun-pandey <varunpandeyuttrakhand+chat@gmail>
2022-11-09 23:04:12 +0530

    hyper optic variation 1 added

--- HYPER_OPTIC/Creating_your_first_A_B_TEST_IN_OPTIMIZE/variation1/variation.js
+++ HYPER_OPTIC/Creating_your_first_A_B_TEST_IN_OPTIMIZE/variation1/variation.js
@@ -1,39 +1,53 @@
-(function () {
-  try {
-    /* main variables */
-    var debug = 0;
-    var variation_name = "";
-    var $;
-    /* all Pure helper functions */
+(function() {
+    try {
+        /* main variables */
+        var debug = 0;
+        var variation_name = "";
+        var $;
+        /* all Pure helper functions */
 
-    function waitForElement(selector, trigger, delayInterval, delayTimeout) {
-      var interval = setInterval(function () {
-        if (
-          document &&
-          document.querySelector(selector) &&
-          document.querySelectorAll(selector).length > 0
-        ) {
-          clearInterval(interval);
-          trigger();
+        function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+            var interval = setInterval(function() {
+                if (
+                    document &&
+                    document.querySelector(selector) &&
+                    document.querySelectorAll(selector).length > 0
+                ) {
+                    clearInterval(interval);
+                    trigger();
+                }
+            }, delayInterval);
+            setTimeout(function() {
+                clearInterval(interval);
+            }, delayTimeout);
         }
-      }, delayInterval);
-      setTimeout(function () {
-        clearInterval(interval);
-      }, delayTimeout);
-    }
 
-    
-    /* Variation Init */
-    function init() {
-      /* start your code here */
-      
-    }
 
-    /* Initialize variation */
-    waitForElement('', init, 50, 15000);
-  } catch (e) {
-    if (debug) console.log(e, "error in Test" + variation_name);
-  }
+        /* Variation Init */
+        function init() {
+            /* start your code here */
+            // adding description 
+
+            const egDesc = [
+                "Great for streaming and wifi",
+                "Perfect for gaming and downloading on multiple devices",
+                "Keeps heavy-user households with lots of gadgets happy",
+                "The ultimate experience, whatever you throw at it."
+            ]
+            setTimeout(() => {
+                const egPriceInfoBoxes = document.querySelectorAll("#block_627dfcc65e214 > .container .packages-wr .package > .package-body > div > .speed-details-wr")
+
+                egPriceInfoBoxes.forEach((priceInfo, index) => {
+                    priceInfo.insertAdjacentHTML("beforebegin", `<p class="eg-desc">${egDesc[index]}</p>`);
+                    console.log(priceInfo)
+                });
+            }, 2000);
+
+        }
+
+        /* Initialize variation */
+        waitForElement('#block_627dfcc65e214 > .container .packages-wr .package > .package-body > div > .speed-details-wr', init, 50, 15000);
+    } catch (e) {
+        if (debug) console.log(e, "error in Test" + variation_name);
+    }
 })();
-
-
