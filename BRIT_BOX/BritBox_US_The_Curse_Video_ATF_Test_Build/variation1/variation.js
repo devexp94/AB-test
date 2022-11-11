eaceea7d4cb0d6b09b69d398865f5fab94e79663
varun-pandey <varunpandeyuttrakhand+chat@gmail>
2022-11-11 21:17:39 +0530

    britbox updated

--- BRIT_BOX/BritBox_US_The_Curse_Video_ATF_Test_Build/variation1/variation.js
+++ BRIT_BOX/BritBox_US_The_Curse_Video_ATF_Test_Build/variation1/variation.js
@@ -1,52 +1,36 @@
 (function() {
-    try {
-        /* main variables */
-        var debug = 0;
-        var variation_name = '';
+  try {
+      /* main variables */
+      var debug = 0;
+      var variation_name = '';
 
-        /* all Pure helper functions */
+      /* all Pure helper functions */
 
-        function waitForElement(selector, trigger, delayInterval, delayTimeout) {
-            var interval = setInterval(function() {
-                if (document && document.querySelector(selector) && document.querySelectorAll(selector).length > 0) {
-                    clearInterval(interval);
-                    trigger();
-                }
-            }, delayInterval);
-            setTimeout(function() {
-                clearInterval(interval);
-            }, delayTimeout);
-        }
+      function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+          var interval = setInterval(function() {
+              if (document && document.querySelector(selector) && document.querySelectorAll(selector).length > 0) {
+                  clearInterval(interval);
+                  trigger();
+              }
+          }, delayInterval);
+          setTimeout(function() {
+              clearInterval(interval);
+          }, delayTimeout);
+      }
 
-        /* Variation functions */
-
-
-
-        function init() {
-            // insertin a new div in the hero section
-            document.querySelector('#page_block_header > div.section-block').insertAdjacentHTML('afterbegin', `
-            <div class="eg-hero-video"></div`);
-
-
-            // getting newly inserted div element
-            const egHeroVideoBlock = document.querySelector(".eg-hero-video");
-
-            // clone the video iframe element
-            const egVideo = document.querySelector("#element-226  iframe").cloneNode(true);
-
-            // set or change attributes
-            egVideo.setAttribute("id", "eg-video-iframe");
-            egVideo.setAttribute("src","https://player.vimeo.com/video/756540934?title=0&autoplay=1&muted=1&controls=0&loop=1");
-
-            // append this cloned iframe to the newly entered div
-            egHeroVideoBlock.insertAdjacentElement("afterbegin",egVideo); 
+      /* Variation functions */
 
 
 
-        }
-        /* Initialize variation */
-        waitForElement('#page_block_header > div.section-block', init, 50, 15000);
-    } catch (e) {
-        if (debug) console.log(e, 'error in Test' + variation_name);
-    }
+      function init() {
+          // insertin a new div in the hero section
+          document.querySelector('#page_block_header > div.section-block').insertAdjacentHTML('afterbegin', `
+          <div class="eg-video-container"><iframe src="https://player.vimeo.com/video/756540934?title=0&autoplay=1&muted=1&controls=0&sidedock=0&loop=1"></iframe></div`);
+
+      }
+      /* Initialize variation */
+      waitForElement('#page_block_header > div.section-block', init, 50, 15000);
+  } catch (e) {
+      if (debug) console.log(e, 'error in Test' + variation_name);
+  }
 })();
