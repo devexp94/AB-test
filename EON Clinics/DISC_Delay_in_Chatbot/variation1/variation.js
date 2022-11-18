2e715580daea62f3c38426c12a73da5f14ca5f4e
varun-pandey <varunpandeyuttrakhand+chat@gmail>
2022-11-18 16:04:17 +0530

    updated

--- EON Clinics/DISC_Delay_in_Chatbot/variation1/variation.js
+++ EON Clinics/DISC_Delay_in_Chatbot/variation1/variation.js
@@ -29,20 +29,20 @@
 
             // delay of 15 seconds 
             // setTimeout(() => {
-            //     document.querySelector("#podium-prompt").style.visibility = "visible";
+            //     document.querySelector("#podium-website-widget").style.visibility = "visible";
             // }, 15000);
 
             // scroll depth
             window.addEventListener("scroll",(e)=>{
               if ((window.innerHeight + window.scrollY + 25) >= document.body.offsetHeight) {
                     // you're at the bottom of the page
-                    document.querySelector("#podium-prompt").style.visibility = "visible";
+                    document.querySelector("#podium-website-widget").style.visibility = "visible";
                 }
             });
         }
 
         /* Initialize variation */
-        waitForElement('#podium-prompt', init, 50, 15000);
+        waitForElement('#podium-website-widget', init, 50, 15000);
     } catch (e) {
         if (debug) console.log(e, "error in Test" + variation_name);
     }
