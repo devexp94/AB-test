0992e72298cb45fbdae7b1d1dc4a1805db566203
varun-pandey <varunpandeyuttrakhand+chat@gmail>
2022-11-18 14:13:51 +0530

    eon clinics updated

--- EON Clinics/DISC_Delay_in_Chatbot/variation1/variation.js
+++ EON Clinics/DISC_Delay_in_Chatbot/variation1/variation.js
@@ -1,41 +1,49 @@
-(function () {
-  try {
-    /* main variables */
-    var debug = 0;
-    var variation_name = "";
-    var $;
-    /* all Pure helper functions */
+(function() {
+    try {
+        /* main variables */
+        var debug = 0;
+        var variation_name = "";
+        var $;
+        /* all Pure helper functions */
 
-    function waitForElement(selector, trigger, delayInterval, delayTimeout) {
-      var interval = setInterval(function () {
-        if (
-          document &&
-          document.querySelector(selector) &&
-          document.querySelectorAll(selector).length > 0
-        ) {
-          clearInterval(interval);
-          trigger();
+        function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+            var interval = setInterval(function() {
+                if (
+                    document &&
+                    document.querySelector(selector) &&
+                    document.querySelectorAll(selector).length > 0
+                ) {
+                    clearInterval(interval);
+                    trigger();
+                }
+            }, delayInterval);
+            setTimeout(function() {
+                clearInterval(interval);
+            }, delayTimeout);
         }
-      }, delayInterval);
-      setTimeout(function () {
-        clearInterval(interval);
-      }, delayTimeout);
-    }
 
-    
-    /* Variation Init */
-    function init() {
-      /* start your code here */
-        setTimeout(()=>{
-          document.querySelector("#podium-prompt").style.visibility = "visible";
-        },15000);
-    }
 
-    /* Initialize variation */
-    waitForElement('#podium-prompt', init, 50, 15000);
-  } catch (e) {
-    if (debug) console.log(e, "error in Test" + variation_name);
-  }
+        /* Variation Init */
+        function init() {
+            /* start your code here */
+
+            // delay of 15 seconds 
+            // setTimeout(() => {
+            //     document.querySelector("#podium-prompt").style.visibility = "visible";
+            // }, 15000);
+
+            // scroll depth
+            window.addEventListener("scroll",(e)=>{
+              if ((window.innerHeight + window.scrollY + 25) >= document.body.offsetHeight) {
+                    // you're at the bottom of the page
+                    document.querySelector("#podium-prompt").style.visibility = "visible";
+                }
+            });
+        }
+
+        /* Initialize variation */
+        waitForElement('#podium-prompt', init, 50, 15000);
+    } catch (e) {
+        if (debug) console.log(e, "error in Test" + variation_name);
+    }
 })();
-
-
