841500b46e80b1ee3e0137f4c83dec37ce00a929
varun-pandey <varunpandeyuttrakhand+chat@gmail>
2022-11-20 08:26:43 +0530

    equifax task'

---
+++ EQUIFAX/Equifax_Alternative_Homepage_CTAs_Test_Build/variation2/variation.js
@@ -0,0 +1,38 @@
+(function() {
+    try {
+        /* main variables */
+        var debug = 0;
+        var variation_name = "";
+        var $;
+        /* all Pure helper functions */
+
+        function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+            var interval = setInterval(function() {
+                if (
+                    document &&
+                    document.querySelector(selector) &&
+                    document.querySelectorAll(selector).length > 0
+                ) {
+                    clearInterval(interval);
+                    trigger();
+                }
+            }, delayInterval);
+            setTimeout(function() {
+                clearInterval(interval);
+            }, delayTimeout);
+        }
+
+
+        /* Variation Init */
+        function init() {
+            /* start your code here */
+            const egHeroCta = document.querySelector("div.heading-container .heading-content a.cta-x")
+            egHeroCta.textContent =  'Try Equifax Credit Report & Score for FREE*';
+        }
+
+        /* Initialize variation */
+        waitForElement('div.heading-container .heading-content a.cta-x', init, 50, 15000);
+    } catch (e) {
+        if (debug) console.log(e, "error in Test" + variation_name);
+    }
+})();
