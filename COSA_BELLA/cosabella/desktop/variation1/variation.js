622d59761842831121c42763fb8ef45b98a4d6b7
varun-pandey <varunpandeyuttrakhand+chat@gmail>
2022-11-20 09:54:09 +0530

    cosabella task added

---
+++ COSA_BELLA/cosabella/desktop/variation1/variation.js
@@ -0,0 +1,40 @@
+(function() {
+    try {
+        /* main variables */
+        var debug = 0;
+        var variation_name = "";
+        var $;
+        /* all Pure helper functions */
+
+        function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+            var interval = setInterval(function() {
+                if (
+                    document &&
+                    document.querySelector(selector) &&
+                    document.querySelectorAll(selector).length > 0
+                ) {
+                    clearInterval(interval);
+                    trigger();
+                }
+            }, delayInterval);
+            setTimeout(function() {
+                clearInterval(interval);
+            }, delayTimeout);
+        }
+
+
+        /* Variation Init */
+        function init() {
+            /* start your code here */
+            if (window.innerWidth > 767) {
+                document.querySelector(".form--product__add-to-cart button span.js-label-atc_disabled").textContent = "Select your size first";
+            }
+
+        }
+
+        /* Initialize variation */
+        waitForElement('.form--product__add-to-cart button span.js-label-atc_disabled', init, 50, 15000);
+    } catch (e) {
+        if (debug) console.log(e, "error in Test" + variation_name);
+    }
+})();
---
+++ COSA_BELLA/cosabella/mobile/variation2/variation.js
@@ -0,0 +1,39 @@
+(function() {
+    try {
+        /* main variables */
+        var debug = 0;
+        var variation_name = "";
+        var $;
+        /* all Pure helper functions */
+
+        function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+            var interval = setInterval(function() {
+                if (
+                    document &&
+                    document.querySelector(selector) &&
+                    document.querySelectorAll(selector).length > 0
+                ) {
+                    clearInterval(interval);
+                    trigger();
+                }
+            }, delayInterval);
+            setTimeout(function() {
+                clearInterval(interval);
+            }, delayTimeout);
+        }
+
+
+        /* Variation Init */
+        function init() {
+            /* start your code here */
+            if (window.innerWidth < 768) {
+                document.querySelector(".form--product__add-to-cart button span.js-label-atc_disabled").textContent = "Add to Cart";
+            }
+        }
+
+        /* Initialize variation */
+        waitForElement('.form--product__add-to-cart button span.js-label-atc_disabled', init, 50, 15000);
+    } catch (e) {
+        if (debug) console.log(e, "error in Test" + variation_name);
+    }
+})();
