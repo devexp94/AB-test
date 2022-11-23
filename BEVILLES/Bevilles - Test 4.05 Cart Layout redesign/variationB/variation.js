621c49917a2d6510210f104e8a289be76afd3a65
varun-pandey <varunpandeyuttrakhand+chat@gmail>
2022-11-23 22:03:13 +0530

    updated

--- BEVILLES/Bevilles - Test 4.05 Cart Layout redesign/variationB/variation.js
+++ BEVILLES/Bevilles - Test 4.05 Cart Layout redesign/variationB/variation.js
@@ -1,43 +1,39 @@
-(function () {
-  try {
-    /* main variables */
-    var debug = 0;
-    var variation_name = "";
-    /* all Pure helper functions */
-    function waitForElement(selector, trigger, delayInterval, delayTimeout) {
-      var interval = setInterval(function () {
-        if (
-          document &&
-          document.querySelector(selector) &&
-          document.querySelectorAll(selector).length > 0
-        ) {
-          clearInterval(interval);
-          trigger();
+(function() {
+    try {
+        /* main variables */
+        var debug = 0;
+        var variation_name = "";
+        /* all Pure helper functions */
+        function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+            var interval = setInterval(function() {
+                if (
+                    document &&
+                    document.querySelector(selector) &&
+                    document.querySelectorAll(selector).length > 0
+                ) {
+                    clearInterval(interval);
+                    trigger();
+                }
+            }, delayInterval);
+            setTimeout(function() {
+                clearInterval(interval);
+            }, delayTimeout);
         }
-      }, delayInterval);
-      setTimeout(function () {
-        clearInterval(interval);
-      }, delayTimeout);
+
+
+        function init() {
+            const egFreeShip = document.querySelector("#content #shopify-section-cart-template .freeShipMsg");
+
+            // inserting login signup msg
+            egFreeShip.insertAdjacentHTML("beforebegin", `
+              <div class="eg-login-signup-msg">
+                  <p>Have an Account? <a href="#" data-login><strong>Login</strong></a> or <a href="#" data-login><strong>sign up</strong></a></p>
+              </div>
+            `);
+        }
+
+        waitForElement("#content #shopify-section-cart-template", init, 100, 35000);
+    } catch (e) {
+        if (debug) console.log(e, "error in Test" + variation_name);
     }
-
-
-    const eg_ship = `<p class="egShip">Congratulations! You now qualify for <strong>FREE SHIPPING!</strong>.</p>`
-    let img = document.querySelector("#cartform > div.row > div.column.cart-left-section > ul > li.item.border-bottom.cf > div > a")
-    let egIcon = `<div class="eg_icon">
-    <img src="https://img.icons8.com/fluency-systems-filled/48/8caadc/hearts.png"/>
-</div> `
-
-    function init() {
-      document.querySelector("#cartform > div.row > div.cart-right-section").insertAdjacentHTML("beforebegin", eg_ship)
-      img.insertAdjacentHTML("afterend" , egIcon);
-
-      document.querySelector(".eg_icon").addEventListener("click", function() {
-      alert()
-      })
-      }
-
-    waitForElement("#shopify-section-cart-template  div.pageContent", init, 100, 35000);
-  } catch (e) {
-    if (debug) console.log(e, "error in Test" + variation_name);
-  }
 })();
