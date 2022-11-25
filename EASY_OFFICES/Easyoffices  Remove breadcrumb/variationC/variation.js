af5681b2216733debd7ef13d29d6f9f736e9ce40
vijaysf123 <109033340+vijaysf123@users.noreply.github.com>
2022-11-25 21:46:10 +0530

    pushed

---
+++ EASY_OFFICES/Easyoffices  Remove breadcrumb/variationC/variation.js
@@ -0,0 +1,53 @@
+(function () {
+  try {
+    /* main variables */
+    var debug = 0;
+    var variation_name = "";
+    var $;
+    /* all Pure helper functions */
+
+    function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+      var interval = setInterval(function () {
+        if (
+          document &&
+          document.querySelector(selector) &&
+          document.querySelectorAll(selector).length > 0
+        ) {
+          clearInterval(interval);
+          trigger();
+        }
+      }, delayInterval);
+      setTimeout(function () {
+        clearInterval(interval);
+      }, delayTimeout);
+    }
+
+    /* Variation Init */
+    function init() {
+      /* start your code here */
+
+      window.onscroll = function () {
+        if (window.pageYOffset > 0) {
+          document.querySelector("section.breadcrumbs").style.display = "none";
+        } else {
+          document.querySelector("section.breadcrumbs").style.display = "block";
+        }
+
+        if (window.innerWidth < 500) {
+          if (window.pageYOffset > 0) {
+            document.querySelector("#listing > div > div.search-content-main > h1").style.display = "none";
+            document.querySelector("section.filter-nav > div.filters > div:nth-child(1)").style.visibility = "hidden";
+          } else {
+            document.querySelector("#listing > div > div.search-content-main > h1").style.display = "block";
+            document.querySelector("section.filter-nav > div.filters > div:nth-child(1)").style.visibility = "visible";
+          }
+        }
+      };
+    }
+
+    /* Initialize variation */
+    waitForElement("section.breadcrumbs", init, 50, 15000);
+  } catch (e) {
+    if (debug) console.log(e, "error in Test" + variation_name);
+  }
+})();
