2da05919377a971daf5a2e6e3e6f2915f1c98b2a
vijaysf123 <109033340+vijaysf123@users.noreply.github.com>
2022-11-25 09:16:34 +0530

    ok

--- THE_SPANISH_GROUP/AB-test  USP in header on 'certified translations' pages/variationB/variation.js
+++ THE_SPANISH_GROUP/AB-test  USP in header on 'certified translations' pages/variationB/variation.js
@@ -23,27 +23,54 @@
     }
 
     /* Variation functions */
-  
-    let htmlStr = `<h3 class="eg_tile"> Translating Over 90 Languages </br>Inc 5000 Award Winners 2021 & 2022 </h3>`
-    let htmlStr2 = `<h3 class="eg_tile"> <span class="eg_one">Lightning Fast Delivery. Same Day Delivery If Needed</span> <br/> Contracted by Governments</h3>`
-    let htmlStr3 = `<h3 class="eg_tile"> <span class="eg_two">100% Guaranteed Acceptance for Translated Documents for USCIS.<br/> Corporations Recommended by Embassies and Consulates</span></h3>`
 
-function init(){
+    let htmlStr = `<h3 class="eg_tile"> <img src="https://img.icons8.com/material-outlined/24/44aaab/checked--v1.png"/>
+    Translating Over 90 Languages </br><img src="https://img.icons8.com/material-outlined/24/44aaab/checked--v1.png"/>
+    Inc 5000 Award Winners 2021 & 2022 </h3>`;
+    
+    let htmlStr2 = `<h3 class="eg_tile"> <span class="eg_one"><img src="https://img.icons8.com/material-outlined/24/44aaab/checked--v1.png"/>
+Lightning Fast Delivery. Same Day Delivery If Needed</span> <br/><img src="https://img.icons8.com/material-outlined/24/44aaab/checked--v1.png"/>
+Contracted by Governments</h3>`;
 
-  if (window.location.href.indexOf('https://thespanishgroup.org/translations/diploma-and-transcript-spanish-translation/') != -1) {
-    document.querySelector("#header-img > section.template_banner .row div").insertAdjacentHTML("afterend", htmlStr);
-  } 
-  else if (window.location.href.indexOf('https://thespanishgroup.org/translations/certified-translations-for-official-civil-documents/') != -1) {
-    document.querySelector("#header-img > section.template_banner .row div").insertAdjacentHTML("afterend", htmlStr2);
-  } 
-  else if (window.location.href.indexOf('https://thespanishgroup.org/translations/immigration-certificate-translation/') != -1) {
-     document.querySelector("#header-img > section.template_banner .row div").insertAdjacentHTML("afterend", htmlStr3);
-   } 
-}
+let htmlStr3 = `<h3 class="eg_tile"> <span class="eg_two"> <img src="https://img.icons8.com/material-outlined/24/44aaab/checked--v1.png"/>
+100% Guaranteed Acceptance for Translated Documents for USCIS.<br/>  <span class="eg_corp"><img src="https://img.icons8.com/material-outlined/24/44aaab/checked--v1.png"/>
+Corporations Recommended by Embassies and Consulates</span> </span></h3>`;
+
+    function init() {
+      if (
+        window.location.href.indexOf(
+          "https://thespanishgroup.org/translations/diploma-and-transcript-spanish-translation/"
+        ) != -1
+      ) {
+        document
+          .querySelector("#header-img > section.template_banner .row div")
+          .insertAdjacentHTML("afterend", htmlStr);
+      } else if (
+        window.location.href.indexOf(
+          "https://thespanishgroup.org/translations/certified-translations-for-official-civil-documents/"
+        ) != -1
+      ) {
+        document
+          .querySelector("#header-img > section.template_banner .row div")
+          .insertAdjacentHTML("afterend", htmlStr2);
+      } else if (
+        window.location.href.indexOf(
+          "https://thespanishgroup.org/translations/immigration-certificate-translation/"
+        ) != -1
+      ) {
+        document
+          .querySelector("#header-img > section.template_banner .row div")
+          .insertAdjacentHTML("afterend", htmlStr3);
+      }
+    }
     /* Initialize variation */
-    waitForElement("#header-img > section.template_banner .row div", init, 50, 15000);
+    waitForElement(
+      "#header-img > section.template_banner .row div",
+      init,
+      50,
+      15000
+    );
   } catch (e) {
     if (debug) console.log(e, "error in Test" + variation_name);
   }
 })();
-
