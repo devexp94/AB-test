1f0eca124949d39fff68ec72fdd3ac9658d8801d
varun-pandey <varunpandeyuttrakhand+chat@gmail>
2022-11-28 21:15:57 +0530

    updated

--- EQUIFAX/Equifax_Alternative_Homepage_CTAs_Test_Build/variation1/variation.js
+++ EQUIFAX/Equifax_Alternative_Homepage_CTAs_Test_Build/variation1/variation.js
@@ -26,12 +26,18 @@
         /* Variation Init */
         function init() {
             /* start your code here */
-            const egHeroCta = document.querySelector("div.heading-container .heading-content a.cta-x")
-            egHeroCta.textContent = 'START FREE TRIAL';
+
+            const egNavCta = document.querySelector(".nav-sub .buttons a.btn:nth-of-type(1)")
+            const egVideoCta = document.querySelector(".video-block-wrapper .cta-x");
+            const egHeroCta = document.querySelector("div.heading-container .heading-content a.cta-x");
+            
+            [egNavCta,egVideoCta,egHeroCta].forEach(cta=>{
+                cta.textContent = "Start Free Trial";
+            });
         }
 
         /* Initialize variation */
-        waitForElement('div.heading-container .heading-content a.cta-x', init, 50, 15000);
+        waitForElement('.video-block-wrapper .cta-x', init, 50, 15000);
     } catch (e) {
         if (debug) console.log(e, "error in Test" + variation_name);
     }
