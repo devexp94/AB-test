d58f5d01af416af87ce4b48b75678b2b3be2010d
vijaysf123 <109033340+vijaysf123@users.noreply.github.com>
2022-12-04 08:51:26 +0530

    done

--- THE_SPANISH_GROUP/AB-test  USP in header on 'certified translations' pages/variationB/variation.js
+++ THE_SPANISH_GROUP/AB-test  USP in header on 'certified translations' pages/variationB/variation.js
@@ -25,15 +25,15 @@
     /* Variation functions */
 
     let htmlStr = `<h3 class="eg_tile"> <img src="https://img.icons8.com/material-outlined/24/44aaab/checked--v1.png"/>
-    Translating Over 90 Languages </br><img src="https://img.icons8.com/material-outlined/24/44aaab/checked--v1.png"/>
+    Translating Over 90 Languages <img src="https://img.icons8.com/material-outlined/24/44aaab/checked--v1.png"/>
     Inc 5000 Award Winners 2021 & 2022 </h3>`;
     
     let htmlStr2 = `<h3 class="eg_tile"> <span class="eg_one"><img src="https://img.icons8.com/material-outlined/24/44aaab/checked--v1.png"/>
-Lightning Fast Delivery. Same Day Delivery If Needed</span> <br/><img src="https://img.icons8.com/material-outlined/24/44aaab/checked--v1.png"/>
+Lightning Fast Delivery. Same Day Delivery If Needed</span> <img src="https://img.icons8.com/material-outlined/24/44aaab/checked--v1.png"/>
 Contracted by Governments</h3>`;
 
 let htmlStr3 = `<h3 class="eg_tile"> <span class="eg_two"> <img src="https://img.icons8.com/material-outlined/24/44aaab/checked--v1.png"/>
-100% Guaranteed Acceptance for Translated Documents for USCIS.<br/>  <span class="eg_corp"><img src="https://img.icons8.com/material-outlined/24/44aaab/checked--v1.png"/>
+100% Guaranteed Acceptance for Translated Documents for USCIS.<span class="eg_corp"><img src="https://img.icons8.com/material-outlined/24/44aaab/checked--v1.png"/>
 Corporations Recommended by Embassies and Consulates</span> </span></h3>`;
 
     function init() {
