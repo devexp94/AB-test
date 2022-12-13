2adae34136033de818a6b3dd7f9c6ed941d81b4d
vijaysf123 <109033340+vijaysf123@users.noreply.github.com>
2022-12-13 23:34:07 +0530

    clear

--- CLEARSCORE/CLS 3.1 - Accessible benefits - creditscore/variation1/variation.js
+++ CLEARSCORE/CLS 3.1 - Accessible benefits - creditscore/variation1/variation.js
@@ -1,101 +1,110 @@
-(function () {
-  try {
-    /* main variables */
-    var debug = 0;
-    var variation_name = "";
-    var $;
-    /* all Pure helper functions */
 
-    function waitForElement(selector, trigger, delayInterval, delayTimeout) {
-      var interval = setInterval(function () {
-        if (
-          document &&
-          document.querySelector(selector) &&
-          document.querySelectorAll(selector).length > 0
-        ) {
-          clearInterval(interval);
-          trigger();
-        }
-      }, delayInterval);
-      setTimeout(function () {
-        clearInterval(interval);
-      }, delayTimeout);
-    }
+(function (w) {
+	"use strict";
 
+	var tag = "cv-3-1",
+		utils,
+		window = typeof unsafeWindow !== "undefined" ? unsafeWindow : w,
+		isTimedOut,
+		timeout = setTimeout(function () {
+			isTimedOut = true;
+		}, 5000),
+		checkForProjectJS = function () {
+			try {
+				var isLoaded = typeof window["cv-pjs"] !== "undefined";
+				if (isTimedOut) return;
+				else if (!isLoaded) window.requestAnimationFrame(checkForProjectJS);
+				else {
+					clearTimeout(timeout);
 
-    let egOverviewHTML = `
-    <section class="eg-overview-container">
-        <div class="eg-overview-wrapper">
-            <div class="eg-callouts-container">
+					utils = window["cv-pjs"];
 
-                <!--Certified Translation-->
-                <div class="eg-callouts">
+					//window[tag].variation = "variation-1";
+					//window[tag].init();
+					initVariation();
+				}
+			} catch (err) {
+				if (utils) utils.log(err, tag);
+			}
+		};
 
-                <div class="eg_img">
-                   <img src="https://expogrowth.in/wp-content/uploads/2022/12/convicon1.svg" alt="callout-icon"> 
-                </div>
-                     
-                <div class ="eg_rows">
-                   <h3 class="eg-title">Your free credit score & report, updated weekly</h3>
-                   <p class="eg-info">Enter your details once, and get free credit reports forever.</p>
-                   </div>   
-                </div>
+	window.requestAnimationFrame(checkForProjectJS);
 
-                <!--Professional Translation-->
-                <div class="eg-callouts">
-                <div class="eg_img">
-                    <img src="https://expogrowth.in/wp-content/uploads/2022/12/convicon2.svg" alt="callout-icon">
-               </div>
+	function initVariation() {
+		var modClass = `${tag}_module`;
+		utils.waitForElement("#gatsby-focus-wrapper  section[data-qa='hero']", function (heroContent) {
+			utils.applyChanges(`.${modClass}`, function () {
+				var cont = document.createElement("div");
+				cont.innerHTML = `
+					<div class="cv-overview-wrapper">
+						<div class="cv-callouts-container">
+			
+							<!--Certified Translation-->
+							<div class="cv-callouts">
+			
+							<div class="cv-img">
+							   <img src="https://cfactory-img.s3.amazonaws.com/CLS/3.1/convicon1.svg" alt="callout-icon"> 
+							</div>
+								 
+							<div class ="cv-rows">
+							   <h3 class="cv-title">Your free credit score & report,<br/> updated weekly</h3>
+							   <p class="cv-info">Enter your details once, and get free credit reports forever.</p>
+							   </div>   
+							</div>
+			
+							<!--Professional Translation-->
+							<div class="cv-callouts">
+							<div class="cv-img">
+								<img src="https://cfactory-img.s3.amazonaws.com/CLS/3.1/convicon2.svg" alt="callout-icon">
+						   </div>
+			
+						   <div class="cv-rows">
+								<h3 class="cv-title">Support to <br/> improve your score</h3>
+								<p class="cv-info">Get clear, personalised insights about your credit, plus tips on how to improve it.</p>
+			
+								</div>
+							</div>
+			
+							<!--Academic Translation-->
+							<div class="cv-callouts">
+							<div class="cv-img">
+								<img src="https://cfactory-img.s3.amazonaws.com/CLS/3.1/convicon3.svg" alt="callout-icon">
+							 </div>
+							 
+							 <div class="cv-rows">
+								<h3 class="cv-title">The UK’s number 1 free credit <br/> score and report app</h3>
+								<p class="cv-info">Over 12 million people  trust ClearScore to help them with their financial future.</p>
+			
+								</div>
+							</div>
+			
+							<!--90+ Languages Supported-->
+							<div class="cv-callouts">
+			
+							<div class="cv-img">
+								<img src="https://cfactory-img.s3.amazonaws.com/CLS/3.1/convicon4.svg" alt="callout-icon">
+								</div>
+			
+								<div class="cv-rows">
+								<h3 class="cv-title">Personalised and pre-approved <br/> credit offers</h3>
+								<p class="cv-info">Get pre-approved for exclusive offers you won’t see anywhere else.</p>
+								</div>
+							</div>
+						</div>
+					</div>`;
 
-               <div class="eg_rows">
-                    <h3 class="eg-title">Support to <br/> improve your score</h3>
-                    <p class="eg-info">Get clear, personalised insights about your credit, plus tips on how to improve it.</p>
+				cont.classList.add(modClass);
+				cont.classList.add('cv-overview-container');
+                document.body.classList.add(tag)
 
-                    </div>
-                </div>
+				heroContent.insertAdjacentElement("afterend", cont);
+                 document.querySelector("#gatsby-focus-wrapper section[class ^= 'featureSection'] > div[class ^= 'ctaContainer'] > a > div").innerText = "Sign up for free";
 
-                <!--Academic Translation-->
-                <div class="eg-callouts">
-                <div class="eg_img">
-                    <img src="https://expogrowth.in/wp-content/uploads/2022/12/convicon3.svg" alt="callout-icon">
-                 </div>
-                 
-                 <div class="eg_rows">
-                    <h3 class="eg-title">The UK’s number 1 free credit score and report app</h3>
-                    <p class="eg-info">Over 12 million people  trust ClearScore to help them with their financial future.</p>
+				utils.log("module inserted", tag);
+			});
+		});
+		
+	}
 
-                    </div>
-                </div>
-
-                <!--90+ Languages Supported-->
-                <div class="eg-callouts">
-
-                <div class="eg_img">
-                    <img src="https://expogrowth.in/wp-content/uploads/2022/12/convicon4.svg" alt="callout-icon">
-                    </div>
-
-                    <div class="eg_rows">
-                    <h3 class="eg-title">Personalised and pre-approved credit offers</h3>
-                    <p class="eg-info">Get pre-approved for exclusive offers you won’t see anywhere else.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>`;
-    
-    /* Variation Init */
-    function init() {
-      /* start your code here */
-
-      document.querySelector("#gatsby-focus-wrapper section[class ^= 'featureSection'] > div[class ^= 'ctaContainer'] > a > div").innerText = "Sign up for free";
-
-      document.querySelector("#gatsby-focus-wrapper  section[data-qa='hero'] ").insertAdjacentHTML("afterend" , egOverviewHTML )
-
-    }
-
-    /* Initialize variation */
-    waitForElement("#gatsby-focus-wrapper  section[data-qa='hero'] ", init, 50, 15000);
-  } catch (e) {
-    if (debug) console.log(e, "error in Test" + variation_name);
-  }
-})();
+	
+})(window);
