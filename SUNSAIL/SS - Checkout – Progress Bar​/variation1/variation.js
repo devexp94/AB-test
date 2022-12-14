ee2bf444e0ace75a36bb52828be3a5f9e10ccdb7
vijaysf123 <109033340+vijaysf123@users.noreply.github.com>
2022-12-14 11:08:49 +0530

    ok

---
+++ SUNSAIL/SS - Checkout – Progress Bar​/variation1/variation.js
@@ -0,0 +1,90 @@
+
+(function () {
+    try {
+      /* main variables */
+      var debug = 0;
+      var variation_name = "";
+      var $;
+      /* all Pure helper functions */
+  
+      function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+        var interval = setInterval(function () {
+          if (
+            document &&
+            document.querySelector(selector) &&
+            document.querySelectorAll(selector).length > 0
+          ) {
+            clearInterval(interval);
+            trigger();
+          }
+        }, delayInterval);
+        setTimeout(function () {
+          clearInterval(interval);
+        }, delayTimeout);
+      }
+  
+      
+      /* Variation Init */
+      function init() {
+        /* start your code here */
+          document.querySelector("#booking-flow  nav > .step:nth-child(2)").childNodes[2].textContent = "Personal Detail";
+      
+      }
+  
+      listener()
+  
+      function listener() {
+      console.log("root changed");
+        /* These are the modifications: */
+        window.addEventListener("locationchange", function() {
+          console.log("root changed 1");
+            waitForElement('#booking-flow  nav > .step:nth-child(2)', init, 50, 15000);
+            waitForElement('#booking-flow nav > .step > span', progress, 50, 15000);
+         
+        });
+        history.pushState = ((f) =>
+            function pushState() {
+                var ret = f.apply(this, arguments);
+                window.dispatchEvent(new Event("pushstate"));
+                window.dispatchEvent(new Event("locationchange"));
+                return ret;
+            })(history.pushState);
+        history.replaceState = ((f) =>
+            function replaceState() {
+                var ret = f.apply(this, arguments);
+                window.dispatchEvent(new Event("replacestate"));
+                window.dispatchEvent(new Event("locationchange"));
+                return ret;
+            })(history.replaceState);
+        window.addEventListener("popstate", () => {
+            window.dispatchEvent(new Event("locationchange"));
+        });
+    }
+  
+    function progress(){
+  
+      setTimeout(()=>{
+  
+        if (window.location.href.indexOf("https://www.moorings.com/booking/passengers") != -1){
+          document.querySelector("#booking-flow nav > .step:nth-child(1) > span").classList.add("eg-active");
+          console.log("arrived1")
+          } 
+          else if(window.location.href.indexOf("https://www.moorings.com/booking/payment") != -1){
+            document.querySelector("#booking-flow nav > .step:nth-child(1) > span").classList.add("eg-active");
+             document.querySelector("#booking-flow nav > .step:nth-child(2) > span").classList.add("eg-active");
+             console.log("arrived2")
+            }
+            else{
+              console.log("there is some error")
+            }
+      }, 100)
+    }
+  
+      /* Initialize variation */
+      waitForElement('#booking-flow  nav > .step > span', init, 50, 15000);
+      waitForElement('#booking-flow  nav > .step', progress, 50, 15000);
+    } catch (e) {
+      if (debug) console.log(e, "error in Test" + variation_name);
+    }
+  })();
+  
