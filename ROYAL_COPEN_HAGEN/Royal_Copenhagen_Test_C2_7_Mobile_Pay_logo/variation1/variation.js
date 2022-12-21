80cb44b650ddad1dda28dbe9eb6c60aafe05d991
varun-pandey <varunpandeyuttrakhand+chat@gmail>
2022-12-21 16:29:54 +0530

    radio button preselected logic added

--- ROYAL_COPEN_HAGEN/Royal_Copenhagen_Test_C2_7_Mobile_Pay_logo/variation1/variation.js
+++ ROYAL_COPEN_HAGEN/Royal_Copenhagen_Test_C2_7_Mobile_Pay_logo/variation1/variation.js
@@ -21,16 +21,55 @@
                 clearInterval(interval);
             }, delayTimeout);
         }
-        
+
+        function live(selector, event, callback, context) {
+            /****Helper Functions****/
+            // helper for enabling IE 8 event bindings
+            function addEvent(el, type, handler) {
+                if (el.attachEvent) el.attachEvent("on" + type, handler);
+                else el.addEventListener(type, handler);
+            }
+            // matches polyfill
+            this.Element &&
+                (function(ElementPrototype) {
+                    ElementPrototype.matches =
+                        ElementPrototype.matches ||
+                        ElementPrototype.matchesSelector ||
+                        ElementPrototype.webkitMatchesSelector ||
+                        ElementPrototype.msMatchesSelector ||
+                        function(selector) {
+                            var node = this,
+                                nodes = (node.parentNode || node.document).querySelectorAll(selector),
+                                i = -1;
+                            while (nodes[++i] && nodes[i] != node);
+                            return !!nodes[i];
+                        };
+                })(Element.prototype);
+            // live binding helper using matchesSelector
+            function live(selector, event, callback, context) {
+                addEvent(context || document, event, function(e) {
+                    var found,
+                        el = e.target || e.srcElement;
+                    while (el && el.matches && el !== context && !(found = el.matches(selector))) el = el.parentElement;
+                    if (found) callback.call(el, e);
+                });
+            }
+            live(selector, event, callback, context);
+        }
+
+        live('.btn-checkout-continue', 'click', function() {
+            waitForElement(`div[class^='c-checkout-review'] label[for=radio-button-mobilepay]`,init,50,15000);
+        });
+
 
         /* Variation Init */
         function init() {
             /* start your code here */
-
+            document.querySelector("div[class^='c-checkout-review'] label[for=radio-button-mobilepay]").click();
         }
 
         /* Initialize variation */
-        waitForElement(`body`, init, 50, 15000);
+        waitForElement(`div[class^='c-checkout-review'] label[for=radio-button-mobilepay]`, init, 50, 15000);
     } catch (e) {
         if (debug) console.log(e, "error in Test" + variation_name);
     }
