e171c2c7cae59285d55ef6c215e01445c98d554b
vijaysf123 <109033340+vijaysf123@users.noreply.github.com>
2022-12-22 09:14:31 +0530

    changes_made

--- HYPER_OPTIC/HO _Contact-us-page/variation1/variation.js
+++ HYPER_OPTIC/HO _Contact-us-page/variation1/variation.js
@@ -39,11 +39,19 @@
         function init() {
             /* start your code here */
 
-            document.querySelector("h4.contactSupportTeam_Title").innerText = "Contact Our 24/7 support team";
+            document.querySelector("h4.contactSupportTeam_Title").innerText = "Contact our 24/7 support team";
 
             document.querySelector("h4.contactSupportTeam_Title").insertAdjacentHTML("afterend", "<p class='egTxt'>Our support team are on hand 24 hours a day, 7 days a week.</p>");
 
             document.querySelector(" div.container div:nth-child(1) > div > div:nth-child(2) > p:nth-child(2)").innerText = "Email";
+
+            document.querySelector("#conForm h2").innerText = "We're here if you need us" ;
+            document.querySelector("#conForm h2").insertAdjacentHTML("afterend" , "<p class='egFormDesc'>Fill out the form below to contact our 24/7 support team. </p>");
+
+            let checkBox = document.querySelector("#conForm  div.checkbox");
+            let element = document.querySelector("#conForm  div.panel-white  p:nth-child(2)")
+            element.insertAdjacentElement("beforebegin", checkBox);
+   
 
             // inserting boxes
             document.querySelector("#pageContent>div:nth-of-type(9)").insertAdjacentHTML("beforebegin", egBoxesHTML);
