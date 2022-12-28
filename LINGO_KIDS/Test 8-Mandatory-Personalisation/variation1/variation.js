c0f6469e3870fdaa5c39a21b70ff6293b621f305
vijaysf123 <109033340+vijaysf123@users.noreply.github.com>
2022-12-28 19:52:46 +0530

    parentsArea Added

--- LINGO_KIDS/Test 8-Mandatory-Personalisation/variation1/variation.js
+++ LINGO_KIDS/Test 8-Mandatory-Personalisation/variation1/variation.js
@@ -61,12 +61,18 @@
         
         
      if(window.location.href.indexOf("https://app.lingokids.com/es/multifeed") != -1){
-      document.querySelector(".bg-red-vintage .title.title-presentation").innerText = "¿Cuántos años tiene tu hijo/a?"
-     }else {
-       document.querySelector(".bg-red-vintage .title.title-presentation").innerText = "How old is your kid?"
+      document.querySelector(".title.title-presentation").innerText = "¿Cuántos años tiene tu hijo/a?"
+    }
+    else if((window.location.href.indexOf("https://app.lingokids.com/en/multifeed") != -1) || (window.location.href.indexOf("https://app.lingokids.com/multifeed") != -1)) {
+      document.querySelector(".title.title-presentation").innerText = "How old is your kid?"
+    }
+     else if((window.location.href.indexOf("https://app.lingokids.com/parents_area") != -1) || (window.location.href.indexOf("https://app.lingokids.com/en/parents_area") != -1)){
+      document.querySelector(".title.title-presentation").innerText = "Which skills would you like your child to practice?"
      }
-
-          waitForElement('.bg-red-vintage .title.title-presentation', init, 5000, 15000);
+     else if(window.location.href.indexOf("https://app.lingokids.com/es/parents_area") != -1){
+      document.querySelector(".title.title-presentation").innerText = "¿Qué habilidades te gustaría que practicara tu hijo/a?"
+     }
+          waitForElement('.title.title-presentation', init, 3000, 15000);
       });
       history.pushState = ((f) =>
           function pushState() {
@@ -92,21 +98,44 @@
 
       document.body.classList.add("eg-body");
 
-      document.querySelector(".bg-red-vintage .navigator .navigator-content a[data-label='continue']").classList.add("egCss");
+      document.querySelector(".navigator .navigator-content a[data-label='continue']").classList.add("egCss");
+        
+      if(window.location.href.indexOf("https://app.lingokids.com/es/multifeed") != -1){
+        document.querySelector(".title.title-presentation").innerText = "¿Cuántos años tiene tu hijo/a?"
+      }
+      else if((window.location.href.indexOf("https://app.lingokids.com/en/multifeed") != -1) || (window.location.href.indexOf("https://app.lingokids.com/multifeed") != -1)) {
+        document.querySelector(".title.title-presentation").innerText = "How old is your kid?"
+      }
+       else if((window.location.href.indexOf("https://app.lingokids.com/parents_area") != -1) || (window.location.href.indexOf("https://app.lingokids.com/en/parents_area") != -1)){
+        document.querySelector(".title.title-presentation").innerText = "Which skills would you like your child to practice?"
+       }
+       else if(window.location.href.indexOf("https://app.lingokids.com/es/parents_area") != -1){
+        document.querySelector(".title.title-presentation").innerText = "¿Qué habilidades te gustaría que practicara tu hijo/a?"
+       }
 
-     if(window.location.href.indexOf("https://app.lingokids.com/es/multifeed") != -1){
-      document.querySelector(".bg-red-vintage .title.title-presentation").innerText = "¿Cuántos años tiene tu hijo/a?"
-     }else{
-       document.querySelector(".bg-red-vintage .title.title-presentation").innerText = "How old is your kid?"
+     /*** Parents area */
+
+     if((window.location.href.indexOf("https://app.lingokids.com/es/parents_area") != -1) || (window.location.href.indexOf("https://app.lingokids.com/parents_area") != -1) || (window.location.href.indexOf("https://app.lingokids.com/en/parents_area") != -1)){ 
+        parentArea();
+
+        live(".mk-section-parentsArea", "click", function(){
+          if(document.querySelector(".egActive")){
+            document.querySelector(".egActive").classList.remove("egActive");
+          }
+          this.classList.add("egActive");
+          document.querySelector(".navigator .navigator-content a[data-label='continue']").classList.remove("egCss")
+        });
+     }
+     else{
+       live(".content-center .pagination + div div.leading-tight > div", "click", function(){
+         if(document.querySelector(".egActive")){
+           document.querySelector(".egActive").classList.remove("egActive");
+         }
+         this.classList.add("egActive");
+         document.querySelector(".navigator .navigator-content a[data-label='continue']").classList.remove("egCss")
+       });
      }
 
-      live(".bg-red-vintage .content-center .pagination + div div.leading-tight > div", "click", function(){
-        if(document.querySelector(".egActive")){
-          document.querySelector(".egActive").classList.remove("egActive");
-        }
-        this.classList.add("egActive");
-        document.querySelector(".bg-red-vintage .navigator .navigator-content a[data-label='continue']").classList.remove("egCss")
-      });
 
       document.querySelector('.items-center.egActive') && document.querySelector('.items-center.egActive').classList.remove('egActive');
 
@@ -144,6 +173,50 @@
         trackGAEvent('body button', 'click button', 'language selector_es');
       });
     }
+
+    function parentArea() {
+      var textosCards = [];
+      var cards = document.querySelectorAll('.text-presentation');
+      for (let i = 0; i < cards.length; i++) {
+        textosCards[i] = cards[i].innerText;
+      }
+      jQuery('.leading-tight>div').css({ display: 'none' });
+ 
+      var myHtml = `
+      <div class="mk-outer-div flex flex-col justify-between space-x-8 text-center">
+        <div class="space-y-2 mk-section-dbbt">
+          <div class='mk-section-parentsArea'>
+            <img class=" md:max-w-md landscape:max-w-xs" alt="A green check arrow." src="https://lingokids-api-assets-production.imgix.net/web/images/rocket.svg">
+            <p class="text-presentation">
+            `+ textosCards[0] + `
+            </p>
+          </div>
+          <div class='mk-section-parentsArea'>
+            <img class=" md:max-w-md landscape:max-w-xs" alt="A green check arrow." src="https://lingokids-api-assets-production.imgix.net/web/images/collaboration.svg">
+            <p class="text-presentation">
+            `+ textosCards[1] + `
+            </p>
+          </div>
+        </div>
+ 
+        <div class="flex flex-col space-y-2 mk-section-dbbt">
+          <div class='mk-section-parentsArea'>
+            <img class=" md:max-w-md landscape:max-w-xs" alt="A green check arrow." src="https://lingokids-api-assets-production.imgix.net/web/images/messages.svg">
+            <p class="text-presentation">
+            `+ textosCards[2] + `
+            </p>
+          </div>
+          <div class='mk-section-parentsArea'>
+            <img class=" md:max-w-md landscape:max-w-xs" alt="A green check arrow." src="https://lingokids-api-assets-production.imgix.net/web/images/cube-scan.svg">
+            <p class="text-presentation">
+            `+ textosCards[3] + `
+            </p>
+          </div>
+        </div>
+      </div>`;
+      jQuery(myHtml).appendTo(jQuery('.leading-tight'));
+     }
+
 
     /************** GA Events *****************/
     function trackGAEvent(eventCategory, eventAction, eventLabel) {
