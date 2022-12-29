cbf79ad24728f74a1c8266f657119f8e32ad43f1
varunUk09 <varunpandeyuttrakhand+chat@gmail.com>
2022-12-29 12:01:30 +0530

    updated

--- TCS_WORLD_TRAVEL/TCS_Homepage_Search/variationB/trip.js
+++ TCS_WORLD_TRAVEL/TCS_Homepage_Search/variationB/trip.js
@@ -48,8 +48,7 @@
         /* Variation Init */
         function init() {
             /* start your code here */
-            document.addEventListener("DOMContentLoaded", function() {
-                waitForjQuery(function() {
+            waitForjQuery(function() {
                     var $ = window.jQuery;
                     let tripType, destination, date;
                     let egQueryParams = window.location.href.split("?").pop().split("&");
@@ -70,32 +69,31 @@
                     tripType = egSearchQuery["tripType"];
                     date = egSearchQuery["date"];
                     destination = egSearchQuery["destination"];
+                    let egTripTypeEl, egDesEl, egDateEl;
+                    waitForElement('#trip_filter_bar form fieldset:nth-of-type(1) > div > div >div:nth-of-type(3) label[class*="trip-type"]', function() {
+                        egTripTypeEl = $(document).find(`label:contains('${tripType}')`)[0];
+                        waitForElement("#trip_filter_bar form fieldset:nth-of-type(3) > div > div >div label", function() {
+                            egDesEl = $(document).find(`label:contains('${destination}')`)[0];
 
-                    waitForElement('#trip_filter_bar form fieldset:nth-of-type(1) > div > div >div:nth-of-type(2) label', function() {
-                        let egTripTypeEl = $(document).find(`label:contains('${tripType}')`)[0];
-                        if (egTripTypeEl) {
+                            egDateEl = $(document).find(`label:contains('${date}')`)[0];
+
                             egTripTypeEl.click();
-                        }
 
+                            if (!egDesEl.parentElement.querySelector("input[type=checkbox]").checked) {
+                                egDesEl.click();
+                            }
+
+
+                            if (!egDateEl.parentElement.querySelector("input[type=checkbox]").checked) {
+                                egDateEl.click();
+                            }
+
+                        }, 50, 15000);
                     }, 50, 15000);
 
-                    waitForElement("#trip_filter_bar form fieldset:nth-of-type(3) > div > div >div label", function() {
-                        let egDesEl = $(document).find(`label:contains('${destination}')`)[0];
-                        if (egDesEl) {
-                            egDesEl.click();
-                        }
 
-
-                        let egDateEl = $(document).find(`label:contains('${date}')`)[0];
-                        if (egDateEl) {
-                            egDateEl.click();
-                        }
-
-
-                    }, 50, 15000);
 
                 });
-            });
 
 
         }
