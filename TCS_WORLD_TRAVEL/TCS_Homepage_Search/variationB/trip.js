e06a6ff4a7dd0d736866c5a8a09ff0c1ece57ea5
varunUk09 <varunpandeyuttrakhand+chat@gmail.com>
2022-12-29 12:05:32 +0530

    updated

--- TCS_WORLD_TRAVEL/TCS_Homepage_Search/variationB/trip.js
+++ TCS_WORLD_TRAVEL/TCS_Homepage_Search/variationB/trip.js
@@ -48,7 +48,8 @@
         /* Variation Init */
         function init() {
             /* start your code here */
-            waitForjQuery(function() {
+            document.addEventListener("DOMContentLoaded", function() {
+                waitForjQuery(function() {
                     var $ = window.jQuery;
                     let tripType, destination, date;
                     let egQueryParams = window.location.href.split("?").pop().split("&");
@@ -70,7 +71,7 @@
                     date = egSearchQuery["date"];
                     destination = egSearchQuery["destination"];
                     let egTripTypeEl, egDesEl, egDateEl;
-                    waitForElement('#trip_filter_bar form fieldset:nth-of-type(1) > div > div >div:nth-of-type(3) label[class*="trip-type"]', function() {
+                    waitForElement('#trip_filter_bar form fieldset:nth-of-type(1) > div > div >div:nth-of-type(3) label', function() {
                         egTripTypeEl = $(document).find(`label:contains('${tripType}')`)[0];
                         waitForElement("#trip_filter_bar form fieldset:nth-of-type(3) > div > div >div label", function() {
                             egDesEl = $(document).find(`label:contains('${destination}')`)[0];
@@ -94,6 +95,7 @@
 
 
                 });
+            });
 
 
         }
