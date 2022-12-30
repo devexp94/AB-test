ba18cca9eb00498bb63d137c2152ca11846864f6
varunUk09 <varunpandeyuttrakhand+chat@gmail.com>
2022-12-30 20:41:56 +0530

    updated

--- ooredoo/Ooredoo  Data Sim banner/variation1/variation.js
+++ ooredoo/Ooredoo  Data Sim banner/variation1/variation.js
@@ -26,25 +26,24 @@
         /* Variation Init */
         function init() {
             /* start your code here */
-
             if (window.location.href.indexOf("https://www.ooredoo.qa/web/en/") != -1) {
 
-                document.querySelector("#why-ooredoo .slick-track > .slick-slide:nth-child(2) img:nth-of-type(1)").src = "https://www.ooredoo.qa/web/wp-content/uploads/2022/12/HomepageWE.png"
+                document.querySelector("html body #why-ooredoo >.container >.slick-list .slick-track > .slick-slide:nth-child(2) >.ooredoo-carousel__img >a img:nth-of-type(1)").src = "https://www.ooredoo.qa/web/wp-content/uploads/2022/12/HomepageWE.png"
                 if (window.innerWidth < 767) {
-                    document.querySelector("#why-ooredoo .slick-track > .slick-slide:nth-child(2) img:nth-of-type(2)").src = "https://www.ooredoo.qa/web/wp-content/uploads/2022/12/HomepageME.png"
+                    document.querySelector("html body #why-ooredoo >.container >.slick-list .slick-track > .slick-slide:nth-child(2) >.ooredoo-carousel__img >a img:nth-of-type(2)").src = "https://www.ooredoo.qa/web/wp-content/uploads/2022/12/HomepageME.png"
                 }
             } else if (window.location.href.indexOf("https://www.ooredoo.qa/web/ar/") != -1) {
-                document.querySelector("#why-ooredoo .slick-track > .slick-slide:nth-child(2) img:nth-of-type(1)").src = "https://www.ooredoo.qa/web/wp-content/uploads/2022/12/HomepageWA.png";
+                document.querySelector("html body #why-ooredoo >.container >.slick-list .slick-track > .slick-slide:nth-child(2) >.ooredoo-carousel__img >a img:nth-of-type(1)").src = "https://www.ooredoo.qa/web/wp-content/uploads/2022/12/HomepageWA.png";
 
                 if (window.innerWidth < 767) {
-                    document.querySelector("#why-ooredoo .slick-track > .slick-slide:nth-child(2) img:nth-of-type(2)").src = "https://www.ooredoo.qa/web/wp-content/uploads/2022/12/HomepageMA.png"
+                    document.querySelector("html body #why-ooredoo >.container >.slick-list .slick-track > .slick-slide:nth-child(2) >.ooredoo-carousel__img >a img:nth-of-type(2)").src = "https://www.ooredoo.qa/web/wp-content/uploads/2022/12/HomepageMA.png"
                 }
             }
 
         }
 
         /* Initialize variation */
-        waitForElement('#why-ooredoo .slick-track > .slick-slide:nth-child(2) img:nth-of-type(2)', init, 2000, 15000);
+        waitForElement('html body #why-ooredoo >.container >.slick-list .slick-track > .slick-slide:nth-child(2) >.ooredoo-carousel__img >a img:nth-of-type(2)', init,2000, 15000);
     } catch (e) {
         if (debug) console.log(e, "error in Test" + variation_name);
     }
