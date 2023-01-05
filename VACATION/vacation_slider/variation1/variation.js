da11b3e5ddf40380e6eada24a11c2d0fea320123
varunUk09 <varunpandeyuttrakhand+chat@gmail.com>
2023-01-05 13:48:30 +0530

    vacation updated

--- VACATION/vacation_slider/variation1/variation.js
+++ VACATION/vacation_slider/variation1/variation.js
@@ -22,6 +22,27 @@
             }, delayTimeout);
         }
 
+        function waitForEmbla(trigger) {
+            var interval = setInterval(function() {
+                if (window.EmblaCarousel != undefined) {
+                    clearInterval(interval);
+                    trigger();
+                } else {
+                    addEmbla();
+                }
+            }, 50);
+            setTimeout(function() {
+                clearInterval(interval);
+            }, 15000)
+        }
+
+        function addEmbla() {
+            var emblaScript = document.createElement('script');
+            // check for updated jquery cdn
+            emblaScript.src = 'https://unpkg.com/embla-carousel/embla-carousel.umd.js';
+            document.getElementsByTagName('head')[0].appendChild(emblaScript);
+        }
+
         function listener() {
             /* These are the modifications: */
             window.addEventListener("locationchange", function() {
@@ -53,120 +74,101 @@
             });
         }
 
-        /* Variation Init */
-        function init() {
-            /* start your code here */
-            console.log('hW A003 started');
-
-            // add thumbnails
-            var thbs = document.querySelector('.product__hero-carousel-thumbs > div:nth-of-type(3)')
-            var feat = document.createElement('div');
-            feat.classList.add('product__hero-thumb', 'mb1', 'pr',"eg-sld");
-            feat.id = 'thbfeat';
-            feat.innerHTML = '<button aria-label="Select image slide" class="image pa x y top left"><picture><img src="https://expogrowth.in/wp-content/uploads/2023/01/PDP-Carousel-Frame-4.jpg"></picture></button>';
 
 
-            // add dots
-            var dots = document.querySelector('.product__hero-dots > button:nth-of-type(3)')
-            var featd = document.createElement('button');
-            featd.classList.add('product__hero-dot',"eg-sld");
-            featd.setAttribute('aria-label', 'Select image slide');
-            featd.id = 'dotfeat';
+        function live(selector, event, callback, context) {
+            /****Helper Functions****/
+            // helper for enabling IE 8 event bindings
+            function addEvent(el, type, handler) {
+                if (el.attachEvent) el.attachEvent("on" + type, handler);
+                else el.addEventListener(type, handler);
+            }
+            // matches polyfill
+            this.Element &&
+                (function(ElementPrototype) {
+                    ElementPrototype.matches =
+                        ElementPrototype.matches ||
+                        ElementPrototype.matchesSelector ||
+                        ElementPrototype.webkitMatchesSelector ||
+                        ElementPrototype.msMatchesSelector ||
+                        function(selector) {
+                            var node = this,
+                                nodes = (node.parentNode || node.document).querySelectorAll(selector),
+                                i = -1;
+                            while (nodes[++i] && nodes[i] != node);
+                            return !!nodes[i];
+                        };
+                })(Element.prototype);
+            // live binding helper using matchesSelector
+            function live(selector, event, callback, context) {
+                addEvent(context || document, event, function(e) {
+                    var found,
+                        el = e.target || e.srcElement;
+                    while (el && el.matches && el !== context && !(found = el.matches(selector))) el = el.parentElement;
+                    if (found) callback.call(el, e);
+                });
+            }
+            live(selector, event, callback, context);
+        }
 
+        var embla;
 
-            // add carousel image
-            var cars = document.querySelector('.embla__container > div:nth-of-type(3)')
-            var feati = document.createElement('div');
-            feati.classList.add('embla__slide',"eg-sld");
-            feati.id = 'feati';
-            feati.setAttribute('style', 'flex:0 0 100%;height:100%;width:100%;position:relative;');
-            feati.innerHTML = '<div class="image pa x y top left"><picture><source srcset="https://expogrowth.in/wp-content/uploads/2023/01/PDP-Carousel-Frame-4.jpg" media="(min-width: 1000px)"><source srcset="https://expogrowth.in/wp-content/uploads/2023/01/PDP-Carousel-Frame-4.jpg" media="(min-width: 600px)"><img src="https://expogrowth.in/wp-content/uploads/2023/01/PDP-Carousel-Frame-4.jpg"></picture></div>';
+        live(['.product__hero-thumb', '.product__hero-dot'], 'click', function() {
 
-
-            if (window.innerWidth < 800) {
-                // add thumbnail
-                thbs = document.querySelector('.product__hero-carousel-thumbs');
-                thbs.insertAdjacentElement("beforeend", feat);
-
-                // add dots
-                dots = document.querySelector('.product__hero-dots');
-                dots.insertAdjacentElement("beforeend", featd);
-
-                // add carousel image
-                cars = document.querySelector('.embla__container');
-                cars.insertAdjacentElement("beforeend", feati);
-            } else {
-                // add thumbnail
-                thbs.insertAdjacentElement("afterend", feat);
-
-                // add dot
-                dots.insertAdjacentElement("afterend", featd)
-
-                // add carousel image
-                cars.insertAdjacentElement("afterend", feati);
+            if (this.parentElement.children[0] == this) {
+                embla.scrollTo(0, false);
+            } else if (this.parentElement.children[1] == this) {
+                embla.scrollTo(1, false);
+            } else if (this.parentElement.children[2] == this) {
+                embla.scrollTo(2, false);
+            } else if (this.parentElement.children[3] == this) {
+                embla.scrollTo(3, false);
+            } else if (this.parentElement.children[4] == this) {
+                embla.scrollTo(4, false);
+            } else if (this.parentElement.children[5] == this) {
+                embla.scrollTo(5, false);
+            } else if (this.parentElement.children[6] == this) {
+                embla.scrollTo(6, false);
+            } else if (this.parentElement.children[7] == this) {
+                embla.scrollTo(7, false);
             }
 
-            // select slide
-            document.addEventListener('click', function(event) {
-                //console.log('clicked');
-                if (!event.target.matches('.product__hero-thumb img, .product__hero-dots button')) return;
-                //console.log('matched');
-                var slides = cars.querySelectorAll('.embla__slide');
-                for (q = 0; q < slides.length; q++) {
-                    if (slides[q].classList.contains('active')) {
-                        slides[q].classList.remove('active');
-                    }
-                }
-                var pos = '0px';
-                var wdt = document.querySelector('.site__wrapper > .row > .col:first-child').offsetWidth;
-                if (event.target.matches('.product__hero-carousel-thumbs > .product__hero-thumb:first-child img, .product__hero-dots button:first-child')) {
-                    var pos = '0';
-                    document.querySelector('.embla__container .embla__slide:nth-child(1)').classList.add('active');
-                } else if (event.target.matches('.product__hero-carousel-thumbs > .product__hero-thumb:nth-child(2) img, .product__hero-dots button:nth-child(2)')) {
-                    var pos = '-' + wdt + 'px';
-                    document.querySelector('.embla__container .embla__slide:nth-child(2)').classList.add('active');
-                } else if (event.target.matches('.product__hero-carousel-thumbs > .product__hero-thumb:nth-child(3) img, .product__hero-dots button:nth-child(3)')) {
-                    var pos = '-' + wdt * 2 + 'px';
-                    document.querySelector('.embla__container .embla__slide:nth-child(3)').classList.add('active');
-                } else if (event.target.matches('.product__hero-carousel-thumbs > .product__hero-thumb:nth-child(4) img, .product__hero-dots button:nth-child(4)')) {
-                    var pos = '-' + wdt * 3 + 'px';
-                    document.querySelector('.embla__container .embla__slide:nth-child(4)').classList.add('active');
-                } else if (event.target.matches('.product__hero-carousel-thumbs > .product__hero-thumb:nth-child(5) img, .product__hero-dots button:nth-child(5)')) {
-                    var pos = '-' + wdt * 4 + 'px';
-                    document.querySelector('.embla__container .embla__slide:nth-child(5)').classList.add('active');
-                } else if (event.target.matches('.product__hero-carousel-thumbs > .product__hero-thumb:nth-child(6) img, .product__hero-dots button:nth-child(6)')) {
-                    var pos = '-' + wdt * 5 + 'px';
-                    document.querySelector('.embla__container .embla__slide:nth-child(6)').classList.add('active');
-                } else if (event.target.matches('.product__hero-carousel-thumbs > .product__hero-thumb:nth-child(7) img, .product__hero-dots button:nth-child(7)')) {
-                    var pos = '-' + wdt * 6 + 'px';
-                    document.querySelector('.embla__container .embla__slide:nth-child(7)').classList.add('active');
-                } else if (event.target.matches('.product__hero-carousel-thumbs > .product__hero-thumb:nth-child(8) img, .product__hero-dots button:nth-child(8)')) {
-                    var pos = '-' + wdt * 7 + 'px';
-                    document.querySelector('.embla__container .embla__slide:nth-child(8)').classList.add('active');
-                }
-                // console.log(pos);
-                // animation needed
-                document.querySelector(".embla__container").style.cssText = `transform: translate3d(${pos}, 0px, 0px);`;
-                var thumbs = thbs.querySelectorAll('.product__hero-thumb');
-                for (t = 0; t < thumbs.length; t++) {
-                    if (thumbs[t].classList.contains('active')) {
-                        thumbs[t].classList.remove('active');
-                    }
-                }
-                var listdots = dots.querySelectorAll('.product__hero-dots button');
-                for (l = 0; l < listdots.length; l++) {
-                    if (listdots[l].classList.contains('active')) {
-                        listdots[l].classList.remove('active');
-                    }
-                }
-                if (event.target.matches('.product__hero-thumb img')) {
-                    event.target.closest('.product__hero-thumb').classList.add('active');
-                } else {
-                    event.target.classList.add('active');
-                }
-                //console.log('done');
-            }, false);
+            this.parentElement.querySelectorAll(".active").forEach(act => {
+                act.classList.remove("active")
+            })
+            this.classList.add("active");
+        });
 
+        /* Variation Init */
+        function init() {
+            waitForEmbla(function() {
+                var emblaNode = document.querySelector('.embla')
+                var options = { dots: true }
+
+                embla = EmblaCarousel(emblaNode, options);
+                const egSlide = document.createElement("div");
+                egSlide.classList.add("embla__slide", "eg-sld");
+                egSlide.style.cssText = `flex:0 0 100%;height:100%;width:100%;position:relative`
+                'flex:0 0 100%;height:100%;width:100%;position:relative'
+                egSlide.innerHTML = `<div class="image pa x y top left">
+                    <picture>
+                        <source srcset="https://expogrowth.in/wp-content/uploads/2023/01/PDP-Carousel-Frame-4.jpg" media="(min-width: 1000px)">
+                        <source srcset="https://expogrowth.in/wp-content/uploads/2023/01/PDP-Carousel-Frame-4.jpg" media="(min-width: 600px)"><img src="https://expogrowth.in/wp-content/uploads/2023/01/PDP-Carousel-Frame-4.jpg"></picture>
+                </div>`;
+                const egSlideThumb = `<div class="product__hero-thumb mb1 pr"><button aria-label="Select image slide" class="image pa x y top left eg-thumb">
+                    <picture><img alt="video thumbnail" src="https://expogrowth.in/wp-content/uploads/2023/01/PDP-Carousel-Frame-4.jpg?width=300&amp;fit_mode=preserve&amp;time=0"></picture>
+                </button></div>`;
+
+
+                document.querySelector(".product__hero-carousel-thumbs > div:nth-child(3)").insertAdjacentHTML("afterend", egSlideThumb);
+
+                const egSlideDot = `<button aria-label="Select image slide" class="product__hero-dot eg-dot"></button>`;
+                document.querySelector(".product__hero-dots > button:nth-child(3)").insertAdjacentHTML("afterend", egSlideDot);
+
+                document.querySelector(".embla__container > div:nth-child(3)").insertAdjacentElement("afterend",egSlide);
+                embla.reInit();
+
+            });
         }
 
         if (window.location.href.indexOf("https://www.vacation.inc/products/scent") != -1) {
@@ -175,7 +177,7 @@
             }, 50, 15000);
 
             /* Initialize variation */
-            waitForElement('.embla__container', init, 50, 15000);
+            waitForElement('.embla', init, 50, 15000);
         }
         listener();
     } catch (e) {
