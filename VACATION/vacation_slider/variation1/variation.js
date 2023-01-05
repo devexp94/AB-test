a36c16d016c1857924f4af4fae09a36c9963831e
varunUk09 <varunpandeyuttrakhand+chat@gmail.com>
2023-01-05 16:22:03 +0530

    updated

--- VACATION/vacation_slider/variation1/variation.js
+++ VACATION/vacation_slider/variation1/variation.js
@@ -43,6 +43,7 @@
             document.getElementsByTagName('head')[0].appendChild(emblaScript);
         }
 
+
         function listener() {
             /* These are the modifications: */
             window.addEventListener("locationchange", function() {
@@ -113,6 +114,7 @@
 
         var embla;
 
+
         live(['.product__hero-thumb', '.product__hero-dot'], 'click', function() {
 
             if (this.parentElement.children[0] == this) {
@@ -139,41 +141,54 @@
             this.classList.add("active");
         });
 
+       
+
         /* Variation Init */
         function init() {
             waitForEmbla(function() {
-                var emblaNode = document.querySelector('.embla')
-                var options = { dots: true }
+                if (!document.querySelector(".eg-sld")) {
+                    var emblaNode = document.querySelector('.embla')
+                    var options = { dots: true }
 
-                embla = EmblaCarousel(emblaNode, options);
-                const egSlide = document.createElement("div");
-                egSlide.classList.add("embla__slide", "eg-sld");
-                egSlide.style.cssText = `flex:0 0 100%;height:100%;width:100%;position:relative`
-                'flex:0 0 100%;height:100%;width:100%;position:relative'
-                egSlide.innerHTML = `<div class="image pa x y top left">
+                    embla = EmblaCarousel(emblaNode, options);
+                    const egSlide = document.createElement("div");
+                    egSlide.classList.add("embla__slide", "eg-sld");
+                    egSlide.style.cssText = `flex:0 0 100%;height:100%;width:100%;position:relative`
+                    'flex:0 0 100%;height:100%;width:100%;position:relative'
+                    egSlide.innerHTML = `<div class="image pa x y top left">
                     <picture>
                         <source srcset="https://expogrowth.in/wp-content/uploads/2023/01/PDP-Carousel-Frame-4.jpg" media="(min-width: 1000px)">
                         <source srcset="https://expogrowth.in/wp-content/uploads/2023/01/PDP-Carousel-Frame-4.jpg" media="(min-width: 600px)"><img src="https://expogrowth.in/wp-content/uploads/2023/01/PDP-Carousel-Frame-4.jpg"></picture>
                 </div>`;
-                const egSlideThumb = `<div class="product__hero-thumb mb1 pr"><button aria-label="Select image slide" class="image pa x y top left eg-thumb">
+                    const egSlideThumb = `<div class="product__hero-thumb mb1 pr"><button aria-label="Select image slide" class="image pa x y top left eg-thumb">
                     <picture><img alt="video thumbnail" src="https://expogrowth.in/wp-content/uploads/2023/01/PDP-Carousel-Frame-4.jpg?width=300&amp;fit_mode=preserve&amp;time=0"></picture>
                 </button></div>`;
 
 
-                document.querySelector(".product__hero-carousel-thumbs > div:nth-child(3)").insertAdjacentHTML("afterend", egSlideThumb);
+                    document.querySelector(".product__hero-carousel-thumbs > div:nth-child(3)").insertAdjacentHTML("afterend", egSlideThumb);
 
-                const egSlideDot = `<button aria-label="Select image slide" class="product__hero-dot eg-dot"></button>`;
-                document.querySelector(".product__hero-dots > button:nth-child(3)").insertAdjacentHTML("afterend", egSlideDot);
+                    const egSlideDot = `<button aria-label="Select image slide" class="product__hero-dot eg-dot"></button>`;
+                    document.querySelector(".product__hero-dots > button:nth-child(3)").insertAdjacentHTML("afterend", egSlideDot);
 
-                document.querySelector(".embla__container > div:nth-child(3)").insertAdjacentElement("afterend",egSlide);
-                embla.reInit();
+                    document.querySelector(".embla__container > div:nth-child(3)").insertAdjacentElement("afterend", egSlide);
+                    embla.reInit();
 
+                }
             });
         }
 
         if (window.location.href.indexOf("https://www.vacation.inc/products/scent") != -1) {
             waitForElement(".shopping-block-pusher >div:nth-child(2) > div:nth-child(1) ul", function() {
-                document.querySelector(".shopping-block-pusher >div:nth-child(2) > div:nth-child(1) ul").insertAdjacentHTML("beforeend", `<li><em>FREE Air Freshener With Purchase!<em></li>`)
+                if (!document.querySelector(".eg-right-txt")) {
+                    document.querySelector(".shopping-block-pusher >div:nth-child(2) > div:nth-child(1) ul").insertAdjacentHTML("beforeend", `<li class="eg-right-txt"><em>FREE Air Freshener With Purchase!<em></li>`)
+                }
+
+                live(".shopping-block-pusher >div:nth-child(2) > div:nth-child(1) > div > button[title='toggle']", "click", function() {
+                    if (!document.querySelector(".eg-right-txt")) {
+                        document.querySelector(".shopping-block-pusher >div:nth-child(2) > div:nth-child(1) ul").insertAdjacentHTML("beforeend", `<li class="eg-right-txt"><em>FREE Air Freshener With Purchase!<em></li>`)
+                    }
+                });
+
             }, 50, 15000);
 
             /* Initialize variation */
