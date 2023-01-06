d1d6cbfe2925053f462c30afcc4bb51bd706c527
varunUk09 <varunpandeyuttrakhand+chat@gmail.com>
2023-01-06 17:39:31 +0530

    updated

--- EVERY_LIFE/eve.23.001 _ Homepage _Review_CTA_Clicks/variation2/variation.js
+++ EVERY_LIFE/eve.23.001 _ Homepage _Review_CTA_Clicks/variation2/variation.js
@@ -1,61 +1,63 @@
-(function () {
-  if (!document.querySelector('.egMain')){
-  try {
-    /* main variables */
-    var debug = 0;
-    var variation_name = "";
-    var $;
-    /* all Pure helper functions */
+(function() {
+    if (!document.querySelector('.egMain')) {
+        try {
+            /* main variables */
+            var debug = 0;
+            var variation_name = "";
+            var $;
+            /* all Pure helper functions */
 
-    function waitForElement(selector, trigger, delayInterval, delayTimeout) {
-      var interval = setInterval(function () {
-        if (
-          document &&
-          document.querySelector(selector) &&
-          document.querySelectorAll(selector).length > 0
-        ) {
-          clearInterval(interval);
-          trigger();
-        }
-      }, delayInterval);
-      setTimeout(function () {
-        clearInterval(interval);
-      }, delayTimeout);
-    }
+            function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+                var interval = setInterval(function() {
+                    if (
+                        document &&
+                        document.querySelector(selector) &&
+                        document.querySelectorAll(selector).length > 0
+                    ) {
+                        clearInterval(interval);
+                        trigger();
+                    }
+                }, delayInterval);
+                setTimeout(function() {
+                    clearInterval(interval);
+                }, delayTimeout);
+            }
 
-    function listener() {
-      /* These are the modifications: */
-      window.addEventListener("locationchange", function () {
-        if (window.location.href.indexOf("https://www.everlylife.com/overview") !== -1){
-          waitForElement("main section:nth-child(4)", init, 1000, 15000);
-        } else {
-          document.body.classList.remove("egBody");
-          document.querySelector(".egMain").remove();
-        }
-      });
-      history.pushState = ((f) =>
-        function pushState() {
-          var ret = f.apply(this, arguments);
-          window.dispatchEvent(new Event("pushstate"));
-          window.dispatchEvent(new Event("locationchange"));
-          return ret;
-        })(history.pushState);
-      history.replaceState = ((f) =>
-        function replaceState() {
-          var ret = f.apply(this, arguments);
-          window.dispatchEvent(new Event("replacestate"));
-          window.dispatchEvent(new Event("locationchange"));
-          return ret;
-        })(history.replaceState);
-      window.addEventListener("popstate", () => {
-        window.dispatchEvent(new Event("locationchange"));
-      });
-    }
+            function listener() {
+                /* These are the modifications: */
+                window.addEventListener("locationchange", function() {
+                    if (window.location.href.indexOf("https://www.everlylife.com/overview") !== -1) {
+                        waitForElement("main section:nth-child(4)", init, 1000, 15000);
+                    } else {
+                        document.body.classList.remove("egBody");
+                        if (document.querySelector(".egMain")) {
+                            document.querySelector(".egMain").remove();
+                        }
+                    }
+                });
+                history.pushState = ((f) =>
+                    function pushState() {
+                        var ret = f.apply(this, arguments);
+                        window.dispatchEvent(new Event("pushstate"));
+                        window.dispatchEvent(new Event("locationchange"));
+                        return ret;
+                    })(history.pushState);
+                history.replaceState = ((f) =>
+                    function replaceState() {
+                        var ret = f.apply(this, arguments);
+                        window.dispatchEvent(new Event("replacestate"));
+                        window.dispatchEvent(new Event("locationchange"));
+                        return ret;
+                    })(history.replaceState);
+                window.addEventListener("popstate", () => {
+                    window.dispatchEvent(new Event("locationchange"));
+                });
+            }
 
-    listener();
+            listener();
 
 
-    let html = `<section class="egMain">
+            let html = `<section class="egMain">
     <div class="egMainDiv">
     <div class="container">
     <div class="title">
@@ -77,31 +79,34 @@
     </div>
     </section>`;
 
-    /* Variation Init */
-    function init() {
-      /* start your code here */
+            /* Variation Init */
+            function init() {
+                /* start your code here */
 
-        document.body.classList.add("egBody");
-        const egInterval = setInterval(() => {
-          if(!document.querySelector('.egMain')){
-            document.querySelector("main section:nth-child(4)").insertAdjacentHTML("afterend" , html);          
-          }else{
-            clearInterval(egInterval)
-          }
-        }, 100);
+                document.body.classList.add("egBody");
+                const egInterval = setInterval(() => {
+                    if (!document.querySelector('.egMain')) {
+                        document.querySelector("main section:nth-child(4)").insertAdjacentHTML("afterend", html);
+                    } else {
+                        clearInterval(egInterval)
+                    }
+                }, 100);
 
-      } 
+            }
 
-    /* Initialize variation */
-    if ((window.location.href.indexOf("https://www.everlylife.com/overview") !== -1) && (!document.querySelector('.egMain'))) {
-    waitForElement("main section:nth-child(4)", init, 50, 15000);
-    }else {
-      document.body.classList.remove("egBody");
-      document.querySelector(".egMain").remove();
+            /* Initialize variation */
+            if ((window.location.href.indexOf("https://www.everlylife.com/overview") !== -1) && (!document.querySelector('.egMain'))) {
+                waitForElement("main section:nth-child(4)", init, 50, 15000);
+            } else {
+                document.body.classList.remove("egBody");
+                if (document.querySelector(".egMain")) {
+                    document.querySelector(".egMain").remove();
+                }
+
+            }
+
+        } catch (e) {
+            if (debug) console.log(e, "error in Test" + variation_name);
+        }
     }
-
-  } catch (e) {
-    if (debug) console.log(e, "error in Test" + variation_name);
-  }
-}
 })();
