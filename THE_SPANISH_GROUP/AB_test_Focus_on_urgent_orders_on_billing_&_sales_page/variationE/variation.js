8bde8ce0aafeb112c6cd5a9b95eebea2a9b4a3a0
varunUk09 <varunpandeyuttrakhand+chat@gmail.com>
2023-01-06 15:19:47 +0530

    updated

--- THE_SPANISH_GROUP/AB_test_Focus_on_urgent_orders_on_billing_&_sales_page/variationE/variation.js
+++ THE_SPANISH_GROUP/AB_test_Focus_on_urgent_orders_on_billing_&_sales_page/variationE/variation.js
@@ -57,24 +57,71 @@
             live(selector, event, callback, context);
         }
 
+
+        const egSelectHtml = `
+            <span class="eg-select eg-standard eg-active">Standard Order</span>
+            <span class="eg-select eg-same-day">Same Day Delivery Order</span>`;
+
         function egInsertBtn() {
-            console.log("hello");
+
+            let egEle;
+
+            // for desktop
+            if (window.innerWidth > 767) {
+                waitForElement("html body #banner .nfn__container > .nfn__content .nfn__actions button#nfnOrderBtn", function() {
+                    egEle = document.querySelector("html body #banner .nfn__container > .nfn__content .nfn__actions button#nfnOrderBtn");
+                    egEle.innerHTML = egSelectHtml;
+                }, 50, 15000);
+
+            } //for mobile devices
+            else {
+                waitForElement("html body .form_container_mobile .nfn__container > .nfn__content .nfn__actions_mobile button#nfnOrderBtn_mobile", function() {
+                    egEle = document.querySelector("html body .form_container_mobile .nfn__container > .nfn__content .nfn__actions_mobile button#nfnOrderBtn_mobile");
+                    egEle.innerHTML = egSelectHtml;
+                }, 50, 15000);
+            }
+
+            // inserting buttons
+
+
+            live('.eg-select', 'click', function() {
+                this.parentElement.querySelector(".eg-active").classList.remove("eg-active");
+                this.classList.add("eg-active");
+                if (this.classList.contains("eg-same-day")) {
+                    let egHref = this.parentElement.parentElement.href;
+                    if (egHref.indexOf("&egSameDay") == -1) {
+                        this.parentElement.parentElement.href += "&egSameDay";
+                        console.log(this.parentElement.parentElement.href);
+                    }
+                } else if (this.classList.contains("eg-standard")) {
+                    let egHref = this.parentElement.parentElement.href;
+
+                    if (egHref.indexOf("&egSameDay")) {
+                        this.parentElement.parentElement.href = this.parentElement.parentElement.href.replace("&egSameDay", "");
+                    }
+                }
+            });
         }
 
 
         function egChangePreselected() {
-            if (window.location.search.indexOf("egBilling") != -1) {
-                setTimeout(() => {
-                    document.querySelector('#nbsStep2 .nbs__options-list li:last-of-type').click();
-                }, 1000);
+            if (window.location.search.indexOf("egSameDay") != -1) {
+                const egSelectIn = setInterval(()=>{
+                    if(!document.querySelector("#nbsStep2 .nbs__options-list li:last-of-type").classList.contains("active")){
+                        document.querySelector('#nbsStep2 .nbs__options-list li:last-of-type').click();
+                    } else {
+                        clearInterval(egSelectIn);
+                    }
+                },100);
+                
             }
         }
 
         /* Initialize variation */
         if (window.location.href.indexOf("https://thespanishgroup.org/translations") != -1) {
-            waitForElement('.nfn__content .nfn__trust', egInsertBtn, 50, 15000);
+            waitForElement('html body', egInsertBtn, 50, 15000);
         } else if (window.location.href.indexOf("https://thespanishgroup.org/billing") != -1) {
-            waitForElement('#nbsStep2 .nbs__options-list li:last-of-type', egChangePreselected, 50, 15000);
+            waitForElement('#nbsStep2 .nbs__options-list li:last-of-type', egChangePreselected,50, 15000);
         }
     } catch (e) {
         if (debug) console.log(e, "error in Test" + variation_name);
