bde1c25c18c985acab4e1e720da8c24b2c478b8a
varunUk09 <varunpandeyuttrakhand+chat@gmail.com>
2023-01-09 18:41:27 +0530

    add functionality added

--- VACATION/AB042_Cart_cross_sell_formatting/variation2/variation.js
+++ VACATION/AB042_Cart_cross_sell_formatting/variation2/variation.js
@@ -93,6 +93,8 @@
                 waitForElement('#__next .cart > div:nth-child(2) >div', init, 50, 15000);
             } else if ((this.classList.contains("eg-comp-product__cta")) || (this.parentElement.classList.contains("eg-comp-product__cta"))) {
                 // add to cart functionality
+                let varientId = this.accessKey || this.parentElement.accessKey;
+                let varientName = this.closest(".eg-comp-product").querySelector(".eg-comp-product__name-price").childNodes[0];
                 fetch("https://checkout.vacation.inc/api/2020-07/graphql", {
                         "headers": {
                             "accept": "application/json",
@@ -110,7 +112,7 @@
                         },
                         "referrer": "https://www.vacation.inc/",
                         "referrerPolicy": "strict-origin-when-cross-origin",
-                        "body": "{\"query\":\"fragment VariantFragment on ProductVariant  { id,title,price,priceV2 { amount,currencyCode },presentmentPrices (first: 20) { pageInfo { hasNextPage,hasPreviousPage },edges { node { price { amount,currencyCode },compareAtPrice { amount,currencyCode } } } },weight,available: availableForSale,sku,compareAtPrice,compareAtPriceV2 { amount,currencyCode },image { id,src: originalSrc,altText },selectedOptions { name,value },unitPrice { amount,currencyCode },unitPriceMeasurement { measuredType,quantityUnit,quantityValue,referenceUnit,referenceValue } },fragment DiscountApplicationFragment on DiscountApplication  { __typename,targetSelection,allocationMethod,targetType,value { ... on MoneyV2 { amount,currencyCode },... on PricingPercentageValue { percentage } },... on ManualDiscountApplication { title,description },... on DiscountCodeApplication { code,applicable },... on ScriptDiscountApplication { description },... on AutomaticDiscountApplication { title } },fragment AppliedGiftCardFragment on AppliedGiftCard  { amountUsedV2 { amount,currencyCode },balanceV2 { amount,currencyCode },presentmentAmountUsed { amount,currencyCode },id,lastCharacters },fragment VariantWithProductFragment on ProductVariant  { ...VariantFragment,product { id,handle } },fragment UserErrorFragment on UserError  { field,message },fragment CheckoutUserErrorFragment on CheckoutUserError  { field,message,code },fragment MailingAddressFragment on MailingAddress  { id,address1,address2,city,company,country,firstName,formatted,lastName,latitude,longitude,phone,province,zip,name,countryCode: countryCodeV2,provinceCode },fragment CheckoutFragment on Checkout  { id,ready,requiresShipping,note,paymentDue,paymentDueV2 { amount,currencyCode },webUrl,orderStatusUrl,taxExempt,taxesIncluded,currencyCode,totalTax,totalTaxV2 { amount,currencyCode },lineItemsSubtotalPrice { amount,currencyCode },subtotalPrice,subtotalPriceV2 { amount,currencyCode },totalPrice,totalPriceV2 { amount,currencyCode },completedAt,createdAt,updatedAt,email,discountApplications (first: 10) { pageInfo { hasNextPage,hasPreviousPage },edges { node { __typename,...DiscountApplicationFragment } } },appliedGiftCards { ...AppliedGiftCardFragment },shippingAddress { ...MailingAddressFragment },shippingLine { handle,price,priceV2 { amount,currencyCode },title },customAttributes { key,value },order { id,processedAt,orderNumber,subtotalPrice,subtotalPriceV2 { amount,currencyCode },totalShippingPrice,totalShippingPriceV2 { amount,currencyCode },totalTax,totalTaxV2 { amount,currencyCode },totalPrice,totalPriceV2 { amount,currencyCode },currencyCode,totalRefunded,totalRefundedV2 { amount,currencyCode },customerUrl,shippingAddress { ...MailingAddressFragment },lineItems (first: 250) { pageInfo { hasNextPage,hasPreviousPage },edges { cursor,node { title,variant { ...VariantWithProductFragment },quantity,customAttributes { key,value } } } } },lineItems (first: 250) { pageInfo { hasNextPage,hasPreviousPage },edges { cursor,node { id,title,variant { ...VariantWithProductFragment },quantity,customAttributes { key,value },discountAllocations { allocatedAmount { amount,currencyCode },discountApplication { __typename,...DiscountApplicationFragment } } } } } },mutation ($checkoutId:ID!,$lineItems:[CheckoutLineItemInput!]!)  { checkoutLineItemsAdd (checkoutId: $checkoutId lineItems: $lineItems) { userErrors { ...UserErrorFragment },checkoutUserErrors { ...CheckoutUserErrorFragment },checkout { ...CheckoutFragment } } }\",\"variables\":{\"checkoutId\":\"" + readCookie('vacation_checkout_id') + "\",\"lineItems\":[{\"variantId\":\"Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zOTY3ODk5NTY2MDk3NA==\",\"quantity\":1,\"customAttributes\":[{\"key\":\"_subtitle\",\"value\":\"SPF 30 Sunscreen Lotion\"},{\"key\":\"_sc\",\"value\":\"undefined_undefined_undefined\"}]}]}}",
+                        "body": "{\"query\":\"fragment VariantFragment on ProductVariant  { id,title,price,priceV2 { amount,currencyCode },presentmentPrices (first: 20) { pageInfo { hasNextPage,hasPreviousPage },edges { node { price { amount,currencyCode },compareAtPrice { amount,currencyCode } } } },weight,available: availableForSale,sku,compareAtPrice,compareAtPriceV2 { amount,currencyCode },image { id,src: originalSrc,altText },selectedOptions { name,value },unitPrice { amount,currencyCode },unitPriceMeasurement { measuredType,quantityUnit,quantityValue,referenceUnit,referenceValue } },fragment DiscountApplicationFragment on DiscountApplication  { __typename,targetSelection,allocationMethod,targetType,value { ... on MoneyV2 { amount,currencyCode },... on PricingPercentageValue { percentage } },... on ManualDiscountApplication { title,description },... on DiscountCodeApplication { code,applicable },... on ScriptDiscountApplication { description },... on AutomaticDiscountApplication { title } },fragment AppliedGiftCardFragment on AppliedGiftCard  { amountUsedV2 { amount,currencyCode },balanceV2 { amount,currencyCode },presentmentAmountUsed { amount,currencyCode },id,lastCharacters },fragment VariantWithProductFragment on ProductVariant  { ...VariantFragment,product { id,handle } },fragment UserErrorFragment on UserError  { field,message },fragment CheckoutUserErrorFragment on CheckoutUserError  { field,message,code },fragment MailingAddressFragment on MailingAddress  { id,address1,address2,city,company,country,firstName,formatted,lastName,latitude,longitude,phone,province,zip,name,countryCode: countryCodeV2,provinceCode },fragment CheckoutFragment on Checkout  { id,ready,requiresShipping,note,paymentDue,paymentDueV2 { amount,currencyCode },webUrl,orderStatusUrl,taxExempt,taxesIncluded,currencyCode,totalTax,totalTaxV2 { amount,currencyCode },lineItemsSubtotalPrice { amount,currencyCode },subtotalPrice,subtotalPriceV2 { amount,currencyCode },totalPrice,totalPriceV2 { amount,currencyCode },completedAt,createdAt,updatedAt,email,discountApplications (first: 10) { pageInfo { hasNextPage,hasPreviousPage },edges { node { __typename,...DiscountApplicationFragment } } },appliedGiftCards { ...AppliedGiftCardFragment },shippingAddress { ...MailingAddressFragment },shippingLine { handle,price,priceV2 { amount,currencyCode },title },customAttributes { key,value },order { id,processedAt,orderNumber,subtotalPrice,subtotalPriceV2 { amount,currencyCode },totalShippingPrice,totalShippingPriceV2 { amount,currencyCode },totalTax,totalTaxV2 { amount,currencyCode },totalPrice,totalPriceV2 { amount,currencyCode },currencyCode,totalRefunded,totalRefundedV2 { amount,currencyCode },customerUrl,shippingAddress { ...MailingAddressFragment },lineItems (first: 250) { pageInfo { hasNextPage,hasPreviousPage },edges { cursor,node { title,variant { ...VariantWithProductFragment },quantity,customAttributes { key,value } } } } },lineItems (first: 250) { pageInfo { hasNextPage,hasPreviousPage },edges { cursor,node { id,title,variant { ...VariantWithProductFragment },quantity,customAttributes { key,value },discountAllocations { allocatedAmount { amount,currencyCode },discountApplication { __typename,...DiscountApplicationFragment } } } } } },mutation ($checkoutId:ID!,$lineItems:[CheckoutLineItemInput!]!)  { checkoutLineItemsAdd (checkoutId: $checkoutId lineItems: $lineItems) { userErrors { ...UserErrorFragment },checkoutUserErrors { ...CheckoutUserErrorFragment },checkout { ...CheckoutFragment } } }\",\"variables\":{\"checkoutId\":\"" + readCookie('vacation_checkout_id') + "\",\"lineItems\":[{\"variantId\":\"" + varientId + "\",\"quantity\":1,\"customAttributes\":[{\"key\":\"_subtitle\",\"value\":\"" + varientName + "\"},{\"key\":\"_sc\",\"value\":\"undefined_undefined_undefined\"}]}]}}",
                         "method": "POST",
                         "mode": "cors",
                         "credentials": "omit"
@@ -143,109 +145,64 @@
         // dummy products
 
         const egProducts = {
-            "CLASSIC LOTION SPF 30": {
+            'CLASSIC LOTION SPF 30': {
                 "comp-products": [{
-                        "url": "https://www.vacation.inc/products/classic-whip-spf-30",
-                        "id": 30
+                        "url": "https://www.vacation.inc/products/strawberry-jello-salad",
+                        "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MjQ0ODI4MjEyNDUyMQ=="
                     },
                     {
                         "url": "https://www.vacation.inc/products/scent",
-                        "id": 30
+                        "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zOTY3ODk5NTY2MDk3NA=="
                     },
                     {
                         "url": "https://www.vacation.inc/products/ball-boy-candle",
-                        "id": 30
-                    },
+                        "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80Mzc2NDgyODMwNzY4OQ=="
+                    }
                 ]
             },
             'PARTIAL OCEAN VIEW SUITE': {
                 "comp-products": [{
                         "url": "https://www.vacation.inc/products/chardonnay-oil-spf-30",
-                        "id": 30
+                        "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zOTgxNTU3NTk5NDU0Mg=="
                     },
                     {
                         "url": "https://www.vacation.inc/products/luxury-duo",
-                        "id": 30
+                        "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80NDA5MDQxNDUzMDc5Mw=="
                     },
                     {
                         "url":"https://www.vacation.inc/products/classic-spray-spf-30",
-                        "id": 30
-                    },
-                ]
-            },
-            'SUPER SPRITZER': {
-                "comp-products": [{
-                        "url": "https://www.vacation.inc/products/mineral-lotion-spf-30",
-                        "id": 30
-                    },
-                    {
-                        "url":"https://www.vacation.inc/products/classic-lotion-pack-of-3",
-                        "id": 30
-                    },
-                    {
-                        "url":"https://www.vacation.inc/products/summer-leisure-pack",
-                        "id": 30
-                    },
-                ]
-            },
-            'MINERAL LOTION SPF 30': {
-                "comp-products": [{
-                        "url":"https://www.vacation.inc/products/air-freshener",
-                        "id": 30
-                    },
-                    {
-                        "url":"https://www.vacation.inc/products/classic-whip-air-freshener",
-                        "id": 30
-                    },
-                    {
-                        "url":"https://www.vacation.inc/products/chardonnay-air-freshener",
-                        "id": 30
-                    },
-                ]
-            },
-            'HOME RESORT THREE CANDLE SET': {
-                "comp-products": [{
-                        "url":"https://www.vacation.inc/products/suite-candle",
-                        "id": 30
-                    },
-                    {
-                        "url":"https://www.vacation.inc/products/lobby-candle",
-                        "id": 30
-                    },
-                    {
-                        "url":"https://www.vacation.inc/products/pool-boy-candle",
-                        "id": 30
-                    },
+                        "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MjgxNDk4MTMwODY0OQ=="
+                    }
                 ]
             },
             'LIP DESSERTS 5X COLLECTOR’S SET': {
                 "comp-products": [{
                         "url":"https://www.vacation.inc/products/strawberry-jello-salad",
-                        "id": 30
+                        "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MjQ0ODI4MjEyNDUyMQ=="
                     },
                     {
                         "url":"https://www.vacation.inc/products/bananas-foster",
-                        "id": 30
+                        "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MjQ0ODIzNDM4MTU0NQ=="
                     },
                     {
                         "url":"https://www.vacation.inc/products/bombe-alaska",
-                        "id": 30
-                    },
+                        "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MjQ0ODMxODU2MjUzNw=="
+                    }
                 ]
             },
             'PINEAPPLE UPSIDE-DOWN CAKE': {
                 "comp-products": [{
                         "url":"https://www.vacation.inc/products/air-freshener-4-pack",
-                        "id": 30
+                        "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MjgwNjYyMzQzNzAzMw=="
                     },
                     {
                         "url":"https://www.vacation.inc/products/air-freshener-variety-pack-pack-of-4",
-                        "id": 30
+                        "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MzMyMDU2ODUxMjc0NQ=="
                     },
                     {
-                        "url":"https://www.vacation.inc/products/scent-sample",
-                        "id": 30
-                    },
+                        "url":"https://www.vacation.inc/products/poolsuite-hat",
+                        "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MDEzNzM1MjE1MTIxNA=="
+                    }
                 ]
             }
         }
@@ -257,7 +214,7 @@
                 let egComp;
                 let egTxts = document.querySelectorAll("#__next .cart .cart__item .cart__item__content >div >div >div >div .cart__content:nth-of-type(1)");
                 for (let i = 0; i < egTxts.length; i++) {
-                    egComp = egProducts[egTxts[i].innerText.toUpperCase()]["comp-products"];
+                    egComp = egProducts[egTxts[i].innerText.toUpperCase()] && egProducts[egTxts[i].innerText.toUpperCase()]["comp-products"];
                     if (egComp) {
                         break;
                     }
@@ -270,7 +227,8 @@
                     // making request
                     async function getData() {
                         for (let i = 0; i < egComp.length; i++) {
-                            url = egComp[i].url;
+                            let url = egComp[i].url;
+                            let varientId = egComp[i].id;
                             const response = await fetch(url);
                             const responseText = await response.text();
                             const ele = document.createElement("div");
@@ -278,10 +236,6 @@
                             const egName = ele.querySelector("#__next .shopping-block-pusher >div:nth-child(1) >div>h1").innerText;
                             const egPrice = ele.querySelector("#__next .shopping-block-pusher >div:nth-child(1) >div>h3:has(>span) > span").innerText;
                             const egImg = ele.querySelector("#__next .embla .embla__slide img").src;
-                            // console.log("First com product")
-                            // console.log(egName);
-                            // console.log(egPrice);
-                            // console.log(egImg);
 
                             // if comproduct is in cart list hiding it from suggestion
                             let egIsPresent = contains("#__next .cart .cart__item .cart__item__content >div >div >div >div .cart__content:nth-of-type(1)", egName)
@@ -298,7 +252,7 @@
                                         <p class="eg-comp-product__name-price">
                                             ${egName} <span class="eg-price">${egPrice}</span>
                                         </p>
-                                        <button class="eg-comp-product__cta">
+                                        <button class="eg-comp-product__cta" accesskey="${varientId}">
                                             <span>ADD</span>
                                         </button>
                                     </div>
