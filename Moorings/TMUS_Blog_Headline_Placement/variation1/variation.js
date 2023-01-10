36235a4d4bd133ddcb4305d0fe976b38fc1a7ec1
varunUk09 <varunpandeyuttrakhand+chat@gmail.com>
2023-01-10 12:56:14 +0530

    updated

---
+++ Moorings/TMUS_Blog_Headline_Placement/variation1/variation.js
@@ -0,0 +1,41 @@
+(function () {
+  try {
+    /* main variables */
+    var debug = 0;
+    var variation_name = "";
+    var $;
+    /* all Pure helper functions */
+
+    function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+      var interval = setInterval(function () {
+        if (
+          document &&
+          document.querySelector(selector) &&
+          document.querySelectorAll(selector).length > 0
+        ) {
+          clearInterval(interval);
+          trigger();
+        }
+      }, delayInterval);
+      setTimeout(function () {
+        clearInterval(interval);
+      }, delayTimeout);
+    }
+
+    
+    /* Variation Init */
+    function init() {
+      /* start your code here */
+      if(window.location.pathname === "/blog"){
+        document.body.classList.add("eg-blog-page");
+      }
+    }
+
+    /* Initialize variation */
+    waitForElement('body', init, 50, 15000);
+  } catch (e) {
+    if (debug) console.log(e, "error in Test" + variation_name);
+  }
+})();
+
+
