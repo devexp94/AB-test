3e7014bd1b68360ae896cb273fc37e0db0823f88
vijaysf123 <109033340+vijaysf123@users.noreply.github.com>
2023-01-10 14:34:55 +0530

    video id added

--- BRIT_BOX/BritBox_US_Stonehouse_Test_Build_(Video-ATF)/variation1/variation.js
+++ BRIT_BOX/BritBox_US_Stonehouse_Test_Build_(Video-ATF)/variation1/variation.js
@@ -25,7 +25,7 @@
         function init() {
             // insertin a new div in the hero section
             document.querySelector('#page_block_header > div.section-block').insertAdjacentHTML('afterbegin', `
-            <div class="eg-video-container"><iframe src="https://player.vimeo.com/video/785088512?title=0&autoplay=1&muted=1&controls=0&sidedock=0&loop=1"></iframe></div`);
+            <div class="eg-video-container"><iframe src="https://player.vimeo.com/video/787733389?title=0&autoplay=1&muted=1&controls=0&sidedock=0&loop=1"></iframe></div`);
   
                 // document.querySelector("#element-240").classList.remove("item-absolute");
                 document.querySelector("#element-240").insertAdjacentElement("beforeend" , "body")
