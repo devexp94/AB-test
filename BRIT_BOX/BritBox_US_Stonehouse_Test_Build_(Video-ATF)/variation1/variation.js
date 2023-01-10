67260d257a95f229a8af804219346e6ca80eddff
vijaysf123 <109033340+vijaysf123@users.noreply.github.com>
2023-01-10 13:30:46 +0530

    desktop variation

---
+++ BRIT_BOX/BritBox_US_Stonehouse_Test_Build_(Video-ATF)/variation1/variation.js
@@ -0,0 +1,37 @@
+(function() {
+    try {
+        /* main variables */
+        var debug = 0;
+        var variation_name = '';
+  
+        /* all Pure helper functions */
+  
+        function waitForElement(selector, trigger, delayInterval, delayTimeout) {
+            var interval = setInterval(function() {
+                if (document && document.querySelector(selector) && document.querySelectorAll(selector).length > 0) {
+                    clearInterval(interval);
+                    trigger();
+                }
+            }, delayInterval);
+            setTimeout(function() {
+                clearInterval(interval);
+            }, delayTimeout);
+        }
+  
+        /* Variation functions */
+  
+  
+  
+        function init() {
+            // insertin a new div in the hero section
+            document.querySelector('#page_block_header > div.section-block').insertAdjacentHTML('afterbegin', `
+            <div class="eg-video-container"><iframe src="https://player.vimeo.com/video/785088512?title=0&autoplay=1&muted=1&controls=0&sidedock=0&loop=1"></iframe></div`);
+  
+        }
+        /* Initialize variation */
+        waitForElement('#page_block_header > div.section-block', init, 50, 15000);
+    } catch (e) {
+        if (debug) console.log(e, 'error in Test' + variation_name);
+    }
+  })();
+  
