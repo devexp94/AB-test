2a92e39693ff95cf169b9c788245051dba380b89
varunUk09 <varunpandeyuttrakhand+chat@gmail.com>
2023-01-10 14:58:33 +0530

    postioned absolute the mobile cta

--- BRIT_BOX/BritBox_US_Stonehouse_Test_Build_(Video-ATF)/variation1/variation.js
+++ BRIT_BOX/BritBox_US_Stonehouse_Test_Build_(Video-ATF)/variation1/variation.js
@@ -3,9 +3,9 @@
         /* main variables */
         var debug = 0;
         var variation_name = '';
-  
+
         /* all Pure helper functions */
-  
+
         function waitForElement(selector, trigger, delayInterval, delayTimeout) {
             var interval = setInterval(function() {
                 if (document && document.querySelector(selector) && document.querySelectorAll(selector).length > 0) {
@@ -17,24 +17,26 @@
                 clearInterval(interval);
             }, delayTimeout);
         }
-  
+
         /* Variation functions */
-  
-  
-  
+
+
+
         function init() {
             // insertin a new div in the hero section
             document.querySelector('#page_block_header > div.section-block').insertAdjacentHTML('afterbegin', `
             <div class="eg-video-container"><iframe src="https://player.vimeo.com/video/787733389?title=0&autoplay=1&muted=1&controls=0&sidedock=0&loop=1"></iframe></div`);
-  
-                // document.querySelector("#element-240").classList.remove("item-absolute");
-                document.querySelector("#element-240").insertAdjacentElement("beforeend" , "body")
-
+            if (window.innerWidth < 768) {
+                waitForElement("html body .eg-video-container + div > div:nth-child(2)", function() {
+                    let egEle = document.querySelector("html body .eg-video-container + div > div:nth-child(2)");
+                    egEle.classList.add("eg-mob-cta");
+                    document.body.insertAdjacentElement("beforeend", egEle);
+                }, 50, 15000);
+            }
         }
         /* Initialize variation */
         waitForElement('#page_block_header > div.section-block', init, 50, 15000);
     } catch (e) {
         if (debug) console.log(e, 'error in Test' + variation_name);
     }
-  })();
-  
+})();
